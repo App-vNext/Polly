19ddd18c51744d168a7af750a1ec4dc8a4083f3f
Michael Wolfenden <michael.wolfenden@gmail.com>
2013-06-16 17:25:32 +1000

    Updated version number

--- SolutionVersion.cs
+++ SolutionVersion.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyVersion("1.1.0")]
+[assembly: AssemblyFileVersion("1.1.0")]
