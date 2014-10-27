ecb8782c407aa8cf2929c2b35d9a7bcc6ada89c3
Maurice de Beijer <mauricedb@computer.org>
2014-10-27 21:01:19 +0000

    Removed not needed namespaces

--- src/Polly.Net35/CircuitBreakerSyntax.cs
+++ src/Polly.Net35/CircuitBreakerSyntax.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Threading.Tasks;
 using Polly.CircuitBreaker;
 
 namespace Polly
--- src/Polly.Net35/RetrySyntax.cs
+++ src/Polly.Net35/RetrySyntax.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Polly.Retry;
 
 namespace Polly
