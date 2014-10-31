a4c94e85ff8a8e3a0ce47e938528c5192dd458ee
Maurice de Beijer <mauricedb@computer.org>
2014-10-31 17:29:08 +0000

    Fixed compilation for .NET 3.5

---
+++ src/Polly.Net35/Utilities/Task.cs
@@ -0,0 +1,12 @@
+﻿namespace System.Threading.Tasks
+{
+    /// <summary>
+    /// Fake Task class to satisfly the compiler with the .NET 3.5 framework.
+    /// </summary>
+    internal class Task
+    {
+        private Task()
+        {
+        }
+    }
+}
