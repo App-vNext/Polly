f7ecd76d308ca0fe327bd31585160773a9f3a2f2
Maurice de Beijer <mauricedb@computer.org>
2014-11-08 19:05:37 +0100

    Added a partial class with the async code of RetryPolicy

--- src/Polly.Net45/Retry/RetryPolicyAsync.cs
+++ src/Polly.Net45/Retry/RetryPolicyAsync.cs
@@ -1,20 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace Polly.Retry
 {
     internal static partial class RetryPolicy
     {
-        public static void Implementation(Action action, IEnumerable<ExceptionPredicate> shouldRetryPredicates, Func<IRetryPolicyState> policyStateFactory)
+        public static async Task ImplementationAsync(Func<Task> action,
+            IEnumerable<ExceptionPredicate> shouldRetryPredicates, Func<IRetryPolicyState> policyStateFactory)
         {
-            var policyState = policyStateFactory();
+            IRetryPolicyState policyState = policyStateFactory();
 
             while (true)
             {
                 try
                 {
-                    action();
+                    await action();
                     return;
                 }
                 catch (Exception ex)
