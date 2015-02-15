8ab218da8ce79ccc5ab07c06410921eb8f433b4d
Michael Wolfenden <michael.wolfenden@gmail.com>
2015-02-15 19:03:14 +1000

    Bump version

--- SolutionVersion.cs
+++ SolutionVersion.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("2.2.0")]
-[assembly: AssemblyFileVersion("2.2.0")]
+[assembly: AssemblyVersion("2.2.1")]
+[assembly: AssemblyFileVersion("2.2.1")]
