658e0d14050b60515ccdd62f2a8697457053ed60
yftachman <yftachman@gmail.com>
2015-05-01 00:03:16 +0300

    Added the AfterFInalRetryFailureSyntax class

---
+++ src/Polly.Net45/AfterFinalRetryFailureSyntax.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Polly
+{
+    /// <summary>
+    /// Fluent API for defining an action to run after the final Retry. 
+    /// </summary>
+    public static partial class AfterFinalRetryFailureSyntax
+    {
+        /// <summary>
+        /// Sets the action to run after the final retry
+        /// </summary>
+        /// <returns></returns>
+        public static PolicyBuilder AfterFinalRetryFailure(this PolicyBuilder policyBuilder, Action<Exception> action)
+        {
+            policyBuilder.AfterFinalRetryFailureAction = action;
+            return policyBuilder;
+        }
+
+    }
+}
