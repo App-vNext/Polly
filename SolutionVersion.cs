b6a4d337a4d5601328f39be467a3df6075ebfe6c
Thomas Mentzel <thomas.mentzel@softwarekueche.de>
2015-06-26 07:19:35 +0200

    bumped to version 2.2.3

--- SolutionVersion.cs
+++ SolutionVersion.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("2.2.2")]
-[assembly: AssemblyFileVersion("2.2.2")]
+[assembly: AssemblyVersion("2.2.3")]
+[assembly: AssemblyFileVersion("2.2.3")]
