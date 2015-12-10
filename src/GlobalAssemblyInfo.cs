255b63592057d634656b74add7636a454247ad6f
Joel Hulen <joelhulen@gmail.com>
2015-12-09 16:21:46 -0800

    Update GlobalAssemblyInfo.cs

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -1,9 +1,9 @@
 using System.Reflection;
 
 [assembly: AssemblyProduct("Polly")]
-[assembly: AssemblyCompany("Michael Wolfenden")]
+[assembly: AssemblyCompany("App vNext")]
 [assembly: AssemblyDescription("Polly is a library that allows developers to express transient exception handling policies such as Retry, Retry Forever, Wait and Retry or Circuit Breaker in a fluent manner.")]
-[assembly: AssemblyCopyright("Copyright (c) 2015, Michael Wolfenden")]
+[assembly: AssemblyCopyright("Copyright (c) 2015, App vNext")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
