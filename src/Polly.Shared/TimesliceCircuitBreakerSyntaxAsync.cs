bb53ece28277d025beecfd54d8654b3b89617a93
reisenberger <dmr@ebs.hw.ac.uk>
2016-04-03 21:25:55 +0100

    Add overloads to configure TimesliceCircuitBreaker

---
+++ src/Polly.Shared/TimesliceCircuitBreakerSyntaxAsync.cs
@@ -0,0 +1,194 @@
+﻿#if SUPPORTS_ASYNC
+
+using System;
+using Polly.CircuitBreaker;
+
+namespace Polly
+{
+    /// <summary>
+    /// Fluent API for defining a Circuit Breaker <see cref="Policy"/>. 
+    /// </summary>
+    public static class TimesliceCircuitBreakerSyntaxAsync
+    {
+        /// <summary>
+        /// <para>The circuit will break if, within any timeslice of duration <paramref name="timesliceDuration"/>, the proportion of failures exceeds <paramref name="failureThreshold"/>, provided also that the number of calls through the circuit in the timeslice exceeds <paramref name="minimumThroughput" />. The circuit will stay
+        /// broken for the <paramref name="durationOfBreak" />. Any attempt to execute this policy
+        /// while the circuit is broken, will immediately throw a <see cref="BrokenCircuitException" /> containing the exception
+        /// that broke the cicuit.
+        /// </para>
+        /// <para>If the first action after the break duration period results in an exception, the circuit will break
+        /// again for another <paramref name="durationOfBreak" />, otherwise it will reset.
+        /// </para>
+        /// </summary>
+        /// <param name="policyBuilder">The policy builder.</param>
+        /// <param name="failureThreshold">The failure threshold at which the circuit will break (a number between 0 and 1; eg 0.5 represents a 50% failure threshold.</param>
+        /// <param name="timesliceDuration">the duration of the timeslice over which call failure ratios are assessed.</param>
+        /// <param name="minimumThroughput">The minimum throughput of calls through the circuit which must be exceeded, for the circuit-breaker to come into operation.</param>
+        /// <param name="durationOfBreak">The duration the circuit will stay open before resetting.</param>
+        /// <returns>The policy instance.</returns>
+        /// <remarks>(see "Release It!" by Michael T. Nygard fi)</remarks>
+        /// <exception cref="System.ArgumentOutOfRangeException">exceptionsAllowedBeforeBreaking;Value must be greater than zero.</exception>
+        public static CircuitBreakerPolicy TimesliceCircuitBreakerAsync(this PolicyBuilder policyBuilder, double failureThreshold, TimeSpan timesliceDuration, int minimumThroughput, TimeSpan durationOfBreak)
+        {
+            Action<Exception, TimeSpan> doNothingOnBreak = (_, __) => { };
+            Action doNothingOnReset = () => { };
+
+            return policyBuilder.TimesliceCircuitBreakerAsync(
+               failureThreshold, timesliceDuration, minimumThroughput, 
+               durationOfBreak,
+               doNothingOnBreak,
+               doNothingOnReset
+               );
+        }
+
+        /// <summary>
+        /// <para> Builds a <see cref="Policy"/> that will function like a Circuit Breaker.</para>
+        /// <para>The circuit will break if, within any timeslice of duration <paramref name="timesliceDuration"/>, the proportion of failures exceeds <paramref name="failureThreshold"/>, provided also that the number of calls through the circuit in the timeslice exceeds <paramref name="minimumThroughput" />. The circuit will stay
+        /// broken for the <paramref name="durationOfBreak" />. Any attempt to execute this policy
+        /// while the circuit is broken, will immediately throw a <see cref="BrokenCircuitException" /> containing the exception
+        /// that broke the cicuit.
+        /// </para>
+        /// <para>If the first action after the break duration period results in an exception, the circuit will break
+        /// again for another <paramref name="durationOfBreak" />, otherwise it will reset.
+        /// </para>
+        /// </summary>
+        /// <param name="policyBuilder">The policy builder.</param>
+        /// <param name="failureThreshold">The failure threshold at which the circuit will break (a number between 0 and 1; eg 0.5 represents a 50% failure threshold.</param>
+        /// <param name="timesliceDuration">the duration of the timeslice over which call failure ratios are assessed.</param>
+        /// <param name="minimumThroughput">The minimum throughput of calls through the circuit which must be exceeded, for the circuit-breaker to come into operation.</param>
+        /// <param name="durationOfBreak">The duration the circuit will stay open before resetting.</param>
+        /// <param name="onBreak">The action to call when the circuit transitions to an <see cref="CircuitState.Open"/> state.</param>
+        /// <param name="onReset">The action to call when the circuit resets to a <see cref="CircuitState.Closed"/> state.</param>
+        /// <returns>The policy instance.</returns>
+        /// <remarks>(see "Release It!" by Michael T. Nygard fi)</remarks>
+        /// <exception cref="System.ArgumentOutOfRangeException">exceptionsAllowedBeforeBreaking;Value must be greater than zero.</exception>
+        /// <exception cref="ArgumentNullException">onBreak</exception>
+        /// <exception cref="ArgumentNullException">onReset</exception>
+        public static CircuitBreakerPolicy TimesliceCircuitBreakerAsync(this PolicyBuilder policyBuilder, double failureThreshold, TimeSpan timesliceDuration, int minimumThroughput, TimeSpan durationOfBreak, Action<Exception, TimeSpan> onBreak, Action onReset)
+        {
+            return policyBuilder.TimesliceCircuitBreakerAsync(
+                failureThreshold, timesliceDuration, minimumThroughput, 
+                durationOfBreak,
+                (exception, timespan, context) => onBreak(exception, timespan),
+                context => onReset()
+                );
+        }
+
+        /// <summary>
+        /// <para> Builds a <see cref="Policy"/> that will function like a Circuit Breaker.</para>
+        /// <para>The circuit will break if, within any timeslice of duration <paramref name="timesliceDuration"/>, the proportion of failures exceeds <paramref name="failureThreshold"/>, provided also that the number of calls through the circuit in the timeslice exceeds <paramref name="minimumThroughput" />. The circuit will stay
+        /// broken for the <paramref name="durationOfBreak" />. Any attempt to execute this policy
+        /// while the circuit is broken, will immediately throw a <see cref="BrokenCircuitException" /> containing the exception
+        /// that broke the cicuit.
+        /// </para>
+        /// <para>If the first action after the break duration period results in an exception, the circuit will break
+        /// again for another <paramref name="durationOfBreak" />, otherwise it will reset.
+        /// </para>
+        /// </summary>
+        /// <param name="policyBuilder">The policy builder.</param>
+        /// <param name="failureThreshold">The failure threshold at which the circuit will break (a number between 0 and 1; eg 0.5 represents a 50% failure threshold.</param>
+        /// <param name="timesliceDuration">the duration of the timeslice over which call failure ratios are assessed.</param>
+        /// <param name="minimumThroughput">The minimum throughput of calls through the circuit which must be exceeded, for the circuit-breaker to come into operation.</param>
+        /// <param name="durationOfBreak">The duration the circuit will stay open before resetting.</param>
+        /// <param name="onBreak">The action to call when the circuit transitions to an <see cref="CircuitState.Open"/> state.</param>
+        /// <param name="onReset">The action to call when the circuit resets to a <see cref="CircuitState.Closed"/> state.</param>
+        /// <returns>The policy instance.</returns>
+        /// <remarks>(see "Release It!" by Michael T. Nygard fi)</remarks>
+        /// <exception cref="System.ArgumentOutOfRangeException">exceptionsAllowedBeforeBreaking;Value must be greater than zero.</exception>
+        /// <exception cref="ArgumentNullException">onBreak</exception>
+        /// <exception cref="ArgumentNullException">onReset</exception>
+        public static CircuitBreakerPolicy TimesliceCircuitBreakerAsync(this PolicyBuilder policyBuilder, double failureThreshold, TimeSpan timesliceDuration, int minimumThroughput, TimeSpan durationOfBreak, Action<Exception, TimeSpan, Context> onBreak, Action<Context> onReset)
+        {
+            Action doNothingOnHalfOpen = () => { };
+            return policyBuilder.TimesliceCircuitBreakerAsync(
+                failureThreshold, timesliceDuration, minimumThroughput, 
+                durationOfBreak, 
+                onBreak, 
+                onReset,
+                doNothingOnHalfOpen
+                );
+        }
+
+        /// <summary>
+        /// <para> Builds a <see cref="Policy"/> that will function like a Circuit Breaker.</para>
+        /// <para>The circuit will break if, within any timeslice of duration <paramref name="timesliceDuration"/>, the proportion of failures exceeds <paramref name="failureThreshold"/>, provided also that the number of calls through the circuit in the timeslice exceeds <paramref name="minimumThroughput" />. The circuit will stay
+        /// broken for the <paramref name="durationOfBreak" />. Any attempt to execute this policy
+        /// while the circuit is broken, will immediately throw a <see cref="BrokenCircuitException" /> containing the exception
+        /// that broke the cicuit.
+        /// </para>
+        /// <para>If the first action after the break duration period results in an exception, the circuit will break
+        /// again for another <paramref name="durationOfBreak" />, otherwise it will reset.
+        /// </para>
+        /// </summary>
+        /// <param name="policyBuilder">The policy builder.</param>
+        /// <param name="failureThreshold">The failure threshold at which the circuit will break (a number between 0 and 1; eg 0.5 represents a 50% failure threshold.</param>
+        /// <param name="timesliceDuration">the duration of the timeslice over which call failure ratios are assessed.</param>
+        /// <param name="minimumThroughput">The minimum throughput of calls through the circuit which must be exceeded, for the circuit-breaker to come into operation.</param>
+        /// <param name="durationOfBreak">The duration the circuit will stay open before resetting.</param>
+        /// <param name="onBreak">The action to call when the circuit transitions to an <see cref="CircuitState.Open"/> state.</param>
+        /// <param name="onReset">The action to call when the circuit resets to a <see cref="CircuitState.Closed"/> state.</param>
+        /// <param name="onHalfOpen">The action to call when the circuit transitions to <see cref="CircuitState.HalfOpen"/> state, ready to try action executions again. </param>
+        /// <returns>The policy instance.</returns>
+        /// <remarks>(see "Release It!" by Michael T. Nygard fi)</remarks>
+        /// <exception cref="System.ArgumentOutOfRangeException">exceptionsAllowedBeforeBreaking;Value must be greater than zero.</exception>
+        /// <exception cref="ArgumentNullException">onBreak</exception>
+        /// <exception cref="ArgumentNullException">onReset</exception>
+        public static CircuitBreakerPolicy TimesliceCircuitBreakerAsync(this PolicyBuilder policyBuilder, double failureThreshold, TimeSpan timesliceDuration, int minimumThroughput, TimeSpan durationOfBreak, Action<Exception, TimeSpan> onBreak, Action onReset, Action onHalfOpen)
+        {
+            return policyBuilder.TimesliceCircuitBreakerAsync(
+                failureThreshold, timesliceDuration, minimumThroughput, 
+                durationOfBreak,
+                (exception, timespan, context) => onBreak(exception, timespan),
+                context => onReset(),
+                onHalfOpen
+                );
+        }
+
+        /// <summary>
+        /// <para> Builds a <see cref="Policy"/> that will function like a Circuit Breaker.</para>
+        /// <para>The circuit will break if, within any timeslice of duration <paramref name="timesliceDuration"/>, the proportion of failures exceeds <paramref name="failureThreshold"/>, provided also that the number of calls through the circuit in the timeslice exceeds <paramref name="minimumThroughput" />. The circuit will stay
+        /// broken for the <paramref name="durationOfBreak" />. Any attempt to execute this policy
+        /// while the circuit is broken, will immediately throw a <see cref="BrokenCircuitException" /> containing the exception
+        /// that broke the cicuit.
+        /// </para>
+        /// <para>If the first action after the break duration period results in an exception, the circuit will break
+        /// again for another <paramref name="durationOfBreak" />, otherwise it will reset.
+        /// </para>
+        /// </summary>
+        /// <param name="policyBuilder">The policy builder.</param>
+        /// <param name="failureThreshold">The failure threshold at which the circuit will break (a number between 0 and 1; eg 0.5 represents a 50% failure threshold.</param>
+        /// <param name="timesliceDuration">the duration of the timeslice over which call failure ratios are assessed.</param>
+        /// <param name="minimumThroughput">The minimum throughput of calls through the circuit which must be exceeded, for the circuit-breaker to come into operation.</param>
+        /// <param name="durationOfBreak">The duration the circuit will stay open before resetting.</param>
+        /// <param name="onBreak">The action to call when the circuit transitions to an <see cref="CircuitState.Open"/> state.</param>
+        /// <param name="onReset">The action to call when the circuit resets to a <see cref="CircuitState.Closed"/> state.</param>
+        /// <param name="onHalfOpen">The action to call when the circuit transitions to <see cref="CircuitState.HalfOpen"/> state, ready to try action executions again. </param>
+        /// <returns>The policy instance.</returns>
+        /// <remarks>(see "Release It!" by Michael T. Nygard fi)</remarks>
+        /// <exception cref="System.ArgumentOutOfRangeException">exceptionsAllowedBeforeBreaking;Value must be greater than zero.</exception>
+        /// <exception cref="ArgumentNullException">onBreak</exception>
+        /// <exception cref="ArgumentNullException">onReset</exception>
+        /// <exception cref="ArgumentNullException">onHalfOpen</exception>
+        public static CircuitBreakerPolicy TimesliceCircuitBreakerAsync(this PolicyBuilder policyBuilder, double failureThreshold, TimeSpan timesliceDuration, int minimumThroughput, TimeSpan durationOfBreak, Action<Exception, TimeSpan, Context> onBreak, Action<Context> onReset, Action onHalfOpen)
+        {
+            if (failureThreshold <= 0) throw new ArgumentOutOfRangeException("failureThreshold", "Value must be greater than zero.");
+            if (failureThreshold > 1) throw new ArgumentOutOfRangeException("failureThreshold", "Value must be less than or equal to one.");
+            if (timesliceDuration < TimeSpan.Zero) throw new ArgumentOutOfRangeException("timesliceDuration", "Value must be greater than zero.");
+            if (minimumThroughput <= 0) throw new ArgumentOutOfRangeException("minimumThroughput", "Value must be greater than zero.");
+            if (durationOfBreak < TimeSpan.Zero) throw new ArgumentOutOfRangeException("durationOfBreak", "Value must be greater than zero.");
+
+            if (onBreak == null) throw new ArgumentNullException("onBreak");
+            if (onReset == null) throw new ArgumentNullException("onReset");
+            if (onHalfOpen == null) throw new ArgumentNullException("onHalfOpen");
+
+            var breakerController = new TimesliceCircuitController(failureThreshold, timesliceDuration, minimumThroughput, durationOfBreak, onBreak, onReset, onHalfOpen);
+            return new CircuitBreakerPolicy(
+                (action, context, cancellationToken, continueOnCapturedContext) => CircuitBreakerEngine.ImplementationAsync(action, context, policyBuilder.ExceptionPredicates, breakerController, cancellationToken, continueOnCapturedContext),
+                policyBuilder.ExceptionPredicates,
+                breakerController
+            );
+        }
+    }
+}
+
+#endif
