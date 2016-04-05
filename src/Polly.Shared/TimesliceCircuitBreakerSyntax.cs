15b7b03028ff4f496364c6d478617fef56c86234
reisenberger <dmr@ebs.hw.ac.uk>
2016-04-05 22:18:45 +0100

    TimesliceCircuitBreaker: refine naming and doco
    Refine documentation and parameter naming.

--- src/Polly.Shared/TimesliceCircuitBreakerSyntax.cs
+++ src/Polly.Shared/TimesliceCircuitBreakerSyntax.cs
@@ -10,19 +10,19 @@ namespace Polly
     {
         /// <summary>
         /// <para> Builds a <see cref="Policy"/> that will function like a Circuit Breaker.</para>
-        /// <para>The circuit will break if, within any timeslice of duration <paramref name="timesliceDuration"/>, the proportion of failures exceeds <paramref name="failureThreshold"/>, provided also that the number of calls through the circuit in the timeslice exceeds <paramref name="minimumThroughput" />. The circuit will stay
-        /// broken for the <paramref name="durationOfBreak" />. Any attempt to execute this policy
+        /// <para>The circuit will break if, within any timeslice of duration <paramref name="timesliceDuration"/>, the proportion of actions resulting in a handled exception exceeds <paramref name="failureThreshold"/>, provided also that the number of actions through the circuit in the timeslice is at least <paramref name="minimumThroughput" />. </para>
+        /// <para>The circuit will stay broken for the <paramref name="durationOfBreak" />. Any attempt to execute this policy
         /// while the circuit is broken, will immediately throw a <see cref="BrokenCircuitException" /> containing the exception
-        /// that broke the cicuit.
+        /// that broke the circuit.
         /// </para>
         /// <para>If the first action after the break duration period results in a handled exception, the circuit will break
         /// again for another <paramref name="durationOfBreak" />; if no exception is thrown, the circuit will reset.
         /// </para>
         /// </summary>
         /// <param name="policyBuilder">The policy builder.</param>
-        /// <param name="failureThreshold">The failure threshold at which the circuit will break (a number between 0 and 1; eg 0.5 represents a 50% failure threshold.</param>
-        /// <param name="timesliceDuration">the duration of the timeslice over which call failure ratios are assessed.</param>
-        /// <param name="minimumThroughput">The minimum throughput of calls through the circuit which must be exceeded, for the circuit-breaker to come into operation.</param>
+        /// <param name="failureThreshold">The failure threshold at which the circuit will break (a number between 0 and 1; eg 0.5 represents breaking if 50% or more of actions result in a handled failure).</param>
+        /// <param name="timesliceDuration">The duration of the timeslice over which failure ratios are assessed.</param>
+        /// <param name="minimumThroughput">The minimum throughput: this many actions or more must pass through the circuit in the timeslice, for statistics to be considered significant and the circuit-breaker to come into action.</param>
         /// <param name="durationOfBreak">The duration the circuit will stay open before resetting.</param>
         /// <returns>The policy instance.</returns>
         /// <remarks>(see "Release It!" by Michael T. Nygard fi)</remarks>
@@ -43,19 +43,19 @@ namespace Polly
         }
 
         /// <summary>
-        /// <para>The circuit will break if, within any timeslice of duration <paramref name="timesliceDuration"/>, the proportion of failures exceeds <paramref name="failureThreshold"/>, provided also that the number of calls through the circuit in the timeslice exceeds <paramref name="minimumThroughput" />. The circuit will stay
-        /// broken for the <paramref name="durationOfBreak" />. Any attempt to execute this policy
+        /// <para>The circuit will break if, within any timeslice of duration <paramref name="timesliceDuration"/>, the proportion of actions resulting in a handled exception exceeds <paramref name="failureThreshold"/>, provided also that the number of actions through the circuit in the timeslice is at least <paramref name="minimumThroughput" />. </para>
+        /// <para>The circuit will stay broken for the <paramref name="durationOfBreak" />. Any attempt to execute this policy
         /// while the circuit is broken, will immediately throw a <see cref="BrokenCircuitException" /> containing the exception
-        /// that broke the cicuit.
+        /// that broke the circuit.
         /// </para>
         /// <para>If the first action after the break duration period results in a handled exception, the circuit will break
         /// again for another <paramref name="durationOfBreak" />; if no exception is thrown, the circuit will reset.
         /// </para>
         /// </summary>
         /// <param name="policyBuilder">The policy builder.</param>
-        /// <param name="failureThreshold">The failure threshold at which the circuit will break (a number between 0 and 1; eg 0.5 represents a 50% failure threshold.</param>
-        /// <param name="timesliceDuration">the duration of the timeslice over which call failure ratios are assessed.</param>
-        /// <param name="minimumThroughput">The minimum throughput of calls through the circuit which must be exceeded, for the circuit-breaker to come into operation.</param>
+        /// <param name="failureThreshold">The failure threshold at which the circuit will break (a number between 0 and 1; eg 0.5 represents breaking if 50% or more of actions result in a handled failure.</param>
+        /// <param name="timesliceDuration">The duration of the timeslice over which failure ratios are assessed.</param>
+        /// <param name="minimumThroughput">The minimum throughput: this many actions or more must pass through the circuit in the timeslice, for statistics to be considered significant and the circuit-breaker to come into action.</param>
         /// <param name="durationOfBreak">The duration the circuit will stay open before resetting.</param>
         /// <param name="onBreak">The action to call when the circuit transitions to an <see cref="CircuitState.Open"/> state.</param>
         /// <param name="onReset">The action to call when the circuit resets to a <see cref="CircuitState.Closed"/> state.</param>
@@ -75,19 +75,19 @@ namespace Polly
         }
 
         /// <summary>
-        /// <para>The circuit will break if, within any timeslice of duration <paramref name="timesliceDuration"/>, the proportion of failures exceeds <paramref name="failureThreshold"/>, provided also that the number of calls through the circuit in the timeslice exceeds <paramref name="minimumThroughput" />. The circuit will stay
-        /// broken for the <paramref name="durationOfBreak" />. Any attempt to execute this policy
+        /// <para>The circuit will break if, within any timeslice of duration <paramref name="timesliceDuration"/>, the proportion of actions resulting in a handled exception exceeds <paramref name="failureThreshold"/>, provided also that the number of actions through the circuit in the timeslice is at least <paramref name="minimumThroughput" />. </para>
+        /// <para>The circuit will stay broken for the <paramref name="durationOfBreak" />. Any attempt to execute this policy
         /// while the circuit is broken, will immediately throw a <see cref="BrokenCircuitException" /> containing the exception
-        /// that broke the cicuit.
+        /// that broke the circuit.
         /// </para>
         /// <para>If the first action after the break duration period results in a handled exception, the circuit will break
         /// again for another <paramref name="durationOfBreak" />; if no exception is thrown, the circuit will reset.
         /// </para>
         /// </summary>
         /// <param name="policyBuilder">The policy builder.</param>
-        /// <param name="failureThreshold">The failure threshold at which the circuit will break (a number between 0 and 1; eg 0.5 represents a 50% failure threshold.</param>
-        /// <param name="timesliceDuration">the duration of the timeslice over which call failure ratios are assessed.</param>
-        /// <param name="minimumThroughput">The minimum throughput of calls through the circuit which must be exceeded, for the circuit-breaker to come into operation.</param>
+        /// <param name="failureThreshold">The failure threshold at which the circuit will break (a number between 0 and 1; eg 0.5 represents breaking if 50% or more of actions result in a handled failure.</param>
+        /// <param name="timesliceDuration">The duration of the timeslice over which failure ratios are assessed.</param>
+        /// <param name="minimumThroughput">The minimum throughput: this many actions or more must pass through the circuit in the timeslice, for statistics to be considered significant and the circuit-breaker to come into action.</param>
         /// <param name="durationOfBreak">The duration the circuit will stay open before resetting.</param>
         /// <param name="onBreak">The action to call when the circuit transitions to an <see cref="CircuitState.Open"/> state.</param>
         /// <param name="onReset">The action to call when the circuit resets to a <see cref="CircuitState.Closed"/> state.</param>
@@ -108,19 +108,19 @@ namespace Polly
         }
 
         /// <summary>
-        /// <para>The circuit will break if, within any timeslice of duration <paramref name="timesliceDuration"/>, the proportion of failures exceeds <paramref name="failureThreshold"/>, provided also that the number of calls through the circuit in the timeslice exceeds <paramref name="minimumThroughput" />. The circuit will stay
-        /// broken for the <paramref name="durationOfBreak" />. Any attempt to execute this policy
+        /// <para>The circuit will break if, within any timeslice of duration <paramref name="timesliceDuration"/>, the proportion of actions resulting in a handled exception exceeds <paramref name="failureThreshold"/>, provided also that the number of actions through the circuit in the timeslice is at least <paramref name="minimumThroughput" />. </para>
+        /// <para>The circuit will stay broken for the <paramref name="durationOfBreak" />. Any attempt to execute this policy
         /// while the circuit is broken, will immediately throw a <see cref="BrokenCircuitException" /> containing the exception
-        /// that broke the cicuit.
+        /// that broke the circuit.
         /// </para>
         /// <para>If the first action after the break duration period results in a handled exception, the circuit will break
         /// again for another <paramref name="durationOfBreak" />; if no exception is thrown, the circuit will reset.
         /// </para>
         /// </summary>
         /// <param name="policyBuilder">The policy builder.</param>
-        /// <param name="failureThreshold">The failure threshold at which the circuit will break (a number between 0 and 1; eg 0.5 represents a 50% failure threshold.</param>
-        /// <param name="timesliceDuration">the duration of the timeslice over which call failure ratios are assessed.</param>
-        /// <param name="minimumThroughput">The minimum throughput of calls through the circuit which must be exceeded, for the circuit-breaker to come into operation.</param>
+        /// <param name="failureThreshold">The failure threshold at which the circuit will break (a number between 0 and 1; eg 0.5 represents breaking if 50% or more of actions result in a handled failure.</param>
+        /// <param name="timesliceDuration">The duration of the timeslice over which failure ratios are assessed.</param>
+        /// <param name="minimumThroughput">The minimum throughput: this many actions or more must pass through the circuit in the timeslice, for statistics to be considered significant and the circuit-breaker to come into action.</param>
         /// <param name="durationOfBreak">The duration the circuit will stay open before resetting.</param>
         /// <param name="onBreak">The action to call when the circuit transitions to an <see cref="CircuitState.Open"/> state.</param>
         /// <param name="onReset">The action to call when the circuit resets to a <see cref="CircuitState.Closed"/> state.</param>
@@ -143,19 +143,19 @@ namespace Polly
 
         /// <summary>
         /// <para> Builds a <see cref="Policy" /> that will function like a Circuit Breaker.</para>
-        /// <para>The circuit will break if, within any timeslice of duration <paramref name="timesliceDuration"/>, the proportion of failures exceeds <paramref name="failureThreshold"/>, provided also that the number of calls through the circuit in the timeslice exceeds <paramref name="minimumThroughput" />. The circuit will stay
-        /// broken for the <paramref name="durationOfBreak" />. Any attempt to execute this policy
+        /// <para>The circuit will break if, within any timeslice of duration <paramref name="timesliceDuration"/>, the proportion of actions resulting in a handled exception exceeds <paramref name="failureThreshold"/>, provided also that the number of actions through the circuit in the timeslice is at least <paramref name="minimumThroughput" />. </para>
+        /// <para>The circuit will stay broken for the <paramref name="durationOfBreak" />. Any attempt to execute this policy
         /// while the circuit is broken, will immediately throw a <see cref="BrokenCircuitException" /> containing the exception
-        /// that broke the cicuit.
+        /// that broke the circuit.
         /// </para>
         /// <para>If the first action after the break duration period results in a handled exception, the circuit will break
         /// again for another <paramref name="durationOfBreak" />; if no exception is thrown, the circuit will reset.
         /// </para>
         /// </summary>
         /// <param name="policyBuilder">The policy builder.</param>
-        /// <param name="failureThreshold">The failure threshold at which the circuit will break (a number between 0 and 1; eg 0.5 represents a 50% failure threshold.</param>
-        /// <param name="timesliceDuration">the duration of the timeslice over which call failure ratios are assessed.</param>
-        /// <param name="minimumThroughput">The minimum throughput of calls through the circuit which must be exceeded, for the circuit-breaker to come into operation.</param>
+        /// <param name="failureThreshold">The failure threshold at which the circuit will break (a number between 0 and 1; eg 0.5 represents breaking if 50% or more of actions result in a handled failure.</param>
+        /// <param name="timesliceDuration">The duration of the timeslice over which failure ratios are assessed.</param>
+        /// <param name="minimumThroughput">The minimum throughput: this many actions or more must pass through the circuit in the timeslice, for statistics to be considered significant and the circuit-breaker to come into action.</param>
         /// <param name="durationOfBreak">The duration the circuit will stay open before resetting.</param>
         /// <param name="onBreak">The action to call when the circuit transitions to an <see cref="CircuitState.Open" /> state.</param>
         /// <param name="onReset">The action to call when the circuit resets to a <see cref="CircuitState.Closed" /> state.</param>
