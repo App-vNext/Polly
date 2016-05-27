ca23e02bb219311c231435e4307d21f86483fee4
reisenberger <dmr@ebs.hw.ac.uk>
2016-05-27 19:53:58 +0100

    Add doco for release 4.2.2
    Add documentation for release 4.2.2 (async support for .NET4.0 via
    Microsoft.Bcl.Async)

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 [assembly: AssemblyProduct("Polly")]
 [assembly: AssemblyCompany("App vNext")]
 [assembly: AssemblyDescription("Polly is a library that allows developers to express transient exception handling policies such as Retry, Retry Forever, Wait and Retry or Circuit Breaker in a fluent manner.")]
-[assembly: AssemblyCopyright("Copyright (c) 2015, App vNext")]
+[assembly: AssemblyCopyright("Copyright (c) 2016, App vNext")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
