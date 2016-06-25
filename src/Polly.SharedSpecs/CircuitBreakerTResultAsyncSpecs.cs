67938d05d142cbfde7c62602fc6a4fb259fd2d99
reisenberger <dmr@ebs.hw.ac.uk>
2016-06-25 22:22:44 +0100

    Add CircuitBreakerTResult specs on predicates
    Add CircuitBreakerTResult specs around result and exception predicates
    (including combined)

--- src/Polly.SharedSpecs/CircuitBreakerTResultAsyncSpecs.cs
+++ src/Polly.SharedSpecs/CircuitBreakerTResultAsyncSpecs.cs
@@ -121,6 +121,29 @@ namespace Polly.Specs
         }
 
         [Fact]
+        public async void Should_open_circuit_with_the_last_handled_result_after_specified_number_of_specified_handled_result_with_predicate_have_been_returned()
+        {
+            CircuitBreakerPolicy<ResultClass> breaker = Policy
+                            .HandleResult<ResultClass>(r => r.ResultCode == ResultPrimitive.Fault)
+                            .CircuitBreakerAsync(2, TimeSpan.FromMinutes(1));
+
+            (await breaker.RaiseResultSequenceAsync(new ResultClass(ResultPrimitive.Fault)).ConfigureAwait(false))
+                  .ResultCode.Should().Be(ResultPrimitive.Fault);
+            breaker.CircuitState.Should().Be(CircuitState.Closed);
+
+            (await breaker.RaiseResultSequenceAsync(new ResultClass(ResultPrimitive.Fault)).ConfigureAwait(false))
+                  .ResultCode.Should().Be(ResultPrimitive.Fault);
+            breaker.CircuitState.Should().Be(CircuitState.Open);
+
+            breaker.Awaiting(b => b.RaiseResultSequenceAsync(new ResultClass(ResultPrimitive.Good)))
+                .ShouldThrow<BrokenCircuitException<ResultClass>>()
+                .WithMessage("The circuit is now open and is not allowing calls.")
+                .Where(e => e.Result.ResultCode == ResultPrimitive.Fault);
+
+            breaker.CircuitState.Should().Be(CircuitState.Open);
+        }
+
+        [Fact]
         public async void Should_not_open_circuit_if_result_returned_is_not_the_handled_result()
         {
             CircuitBreakerPolicy<ResultPrimitive> breaker = Policy
@@ -158,6 +181,47 @@ namespace Polly.Specs
 
             (await breaker.RaiseResultSequenceAsync(ResultPrimitive.FaultAgain).ConfigureAwait(false))
                 .Should().Be(ResultPrimitive.FaultAgain);
+            breaker.CircuitState.Should().Be(CircuitState.Closed);
+        }
+
+        [Fact]
+        public async void Should_not_open_circuit_if_result_returned_does_not_match_result_predicate()
+        {
+            CircuitBreakerPolicy<ResultClass> breaker = Policy
+                            .HandleResult<ResultClass>(r => r.ResultCode == ResultPrimitive.Fault)
+                            .CircuitBreakerAsync(2, TimeSpan.FromMinutes(1));
+
+            (await breaker.RaiseResultSequenceAsync(new ResultClass(ResultPrimitive.FaultAgain)).ConfigureAwait(false))
+                .ResultCode.Should().Be(ResultPrimitive.FaultAgain);
+            breaker.CircuitState.Should().Be(CircuitState.Closed);
+
+            (await breaker.RaiseResultSequenceAsync(new ResultClass(ResultPrimitive.FaultAgain)).ConfigureAwait(false))
+                .ResultCode.Should().Be(ResultPrimitive.FaultAgain);
+            breaker.CircuitState.Should().Be(CircuitState.Closed);
+
+            (await breaker.RaiseResultSequenceAsync(new ResultClass(ResultPrimitive.FaultAgain)).ConfigureAwait(false))
+                .ResultCode.Should().Be(ResultPrimitive.FaultAgain);
+            breaker.CircuitState.Should().Be(CircuitState.Closed);
+        }
+
+        [Fact]
+        public async void Should_not_open_circuit_if_result_returned_does_not_match_any_of_the_result_predicates()
+        {
+            CircuitBreakerPolicy<ResultClass> breaker = Policy
+                .HandleResult<ResultClass>(r => r.ResultCode == ResultPrimitive.Fault)
+                .OrResult(r => r.ResultCode == ResultPrimitive.FaultYetAgain)
+                .CircuitBreakerAsync(2, TimeSpan.FromMinutes(1));
+
+            (await breaker.RaiseResultSequenceAsync(new ResultClass(ResultPrimitive.FaultAgain)).ConfigureAwait(false))
+                .ResultCode.Should().Be(ResultPrimitive.FaultAgain);
+            breaker.CircuitState.Should().Be(CircuitState.Closed);
+
+            (await breaker.RaiseResultSequenceAsync(new ResultClass(ResultPrimitive.FaultAgain)).ConfigureAwait(false))
+                .ResultCode.Should().Be(ResultPrimitive.FaultAgain);
+            breaker.CircuitState.Should().Be(CircuitState.Closed);
+
+            (await breaker.RaiseResultSequenceAsync(new ResultClass(ResultPrimitive.FaultAgain)).ConfigureAwait(false))
+                .ResultCode.Should().Be(ResultPrimitive.FaultAgain);
             breaker.CircuitState.Should().Be(CircuitState.Closed);
         }
 
