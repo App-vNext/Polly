a890d76d55ff56990bd55fde3205601b37bbb60c
'Ankit <ankitbko@gmail.com>
2017-03-06 15:17:06 +0530

    Refactored the comments.

--- src/Polly.Shared/Registry/DefaultPolicyRegistry.cs
+++ src/Polly.Shared/Registry/DefaultPolicyRegistry.cs
@@ -6,7 +6,7 @@ using System;
 namespace Polly.Registry
 {
     /// <summary>
-    /// Stores a collection of <see cref="System.String"/> and <see cref="Policy"/> pair.
+    /// Stores a registry of <see cref="System.String"/> and <see cref="Policy"/> pair.
     /// </summary>
     /// <remarks>Uses ConcurrentDictionary to store the collection.</remarks>
     public class DefaultPolicyRegistry : IPolicyRegistry<string, Policy>
@@ -14,12 +14,12 @@ namespace Polly.Registry
         private IDictionary<string, Policy> _registry = new ConcurrentDictionary<string, Policy>();
 
         /// <summary>
-        /// Gets or sets the value associated with the specified key.
+        /// Gets or sets the <see cref="Policy"/> associated with the specified key.
         /// </summary>
-        /// <param name="key">The key of the value to get or set.</param>
-        /// <returns>The value of the key/value pair at the specified index.</returns>
-        /// <exception cref="ArgumentNullException">Key is null</exception>
-        /// <exception cref="KeyNotFoundException">Key does not exist in the collection.</exception>
+        /// <param name="key">The key of the <see cref="Policy"/> to get or set.</param>
+        /// <returns>The <see cref="Policy"/> with specified <paramref name="key"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="key"/> is null.</exception>
+        /// <exception cref="KeyNotFoundException">Policy with the specified <paramref name="key"/> does not exists in the registry.</exception>
         public Policy this[string key]
         {
             get { return _registry[key]; }
@@ -28,7 +28,7 @@ namespace Polly.Registry
         }
 
         /// <summary>
-        /// Gets the number of key/<see cref="Policy"/> pairs stored in the registry.
+        /// Total number of policies in the registry.
         /// </summary>
         public int Count => _registry.Count;
 
@@ -37,45 +37,45 @@ namespace Polly.Registry
         /// </summary>
         /// <param name="key">The string to use as the key of the element to add.</param>
         /// <param name="value">The <see cref="Policy"/> to store in the registry.</param>
-        /// <exception cref="ArgumentNullException"><paramref name="key"/> is null</exception>
-        /// <exception cref="ArgumentException">A Policy with same <paramref name="key"/> already exists</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="key"/> is null.</exception>
+        /// <exception cref="ArgumentException">A Policy with same <paramref name="key"/> already exists.</exception>
         public void Add(string key, Policy value) =>
             _registry.Add(key, value);
 
         /// <summary>
-        /// Removes all keys and values from registry
+        /// Removes all keys and policies from registry.
         /// </summary>
         public void Clear() =>
             _registry.Clear();
 
         /// <summary>
-        /// Determines whether the specified Key exists.
+        /// Determines whether the specified <paramref name="key"/> exists.
         /// </summary>
-        /// <param name="key">The Key to locate in the registry</param>
-        /// <returns>True if Key exists otherwise false</returns>
-        /// <exception cref="ArgumentNullException"><paramref name="key"/> is null</exception>
+        /// <param name="key">The key to locate in the registry.</param>
+        /// <returns>True if <paramref name="key"/> exists otherwise false.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="key"/> is null.</exception>
         public bool ContainsKey(string key) =>
             _registry.ContainsKey(key);
 
         /// <summary>
-        /// Removes the specified <see cref="Policy"/> from the registry
+        /// Removes the stored <see cref="Policy"/> under specified <paramref name="key"/> from the registry.
         /// </summary>
-        /// <param name="key">The key of the policy to remove</param>
-        /// <returns>True if Policy is successfully removed. Otherwise false.</returns>
-        /// <exception cref="ArgumentNullException">key is null.</exception>
+        /// <param name="key">The <paramref name="key"/> of the policy to remove.</param>
+        /// <returns>True if <see cref="Policy"/> is successfully removed. Otherwise false.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="key"/> is null.</exception>
         public bool Remove(string key) =>
             _registry.Remove(key);
 
         /// <summary>
-        /// Gets the Policy associated with the specified key
+        /// Gets the <see cref="Policy"/> associated with the specified key.
         /// </summary>
-        /// <param name="key">The key whose value to get</param>
+        /// <param name="key">The key whose value to get.</param>
         /// <param name="value">
-        /// When this method returns, the Policy associated with the specified key, if the
-        /// key is found; otherwise, the default value for the type of the value parameter.
+        /// This method returns the <see cref="Policy"/> associated with the specified <paramref name="key"/>, if the
+        /// key is found; otherwise null.
         /// This parameter is passed uninitialized.
         /// </param>
-        /// <returns>True if Policy exists for the provided Key. False otherwise</returns>
+        /// <returns>True if Policy exists for the provided Key. False otherwise.</returns>
         public bool TryGetValue(string key, out Policy value) =>
             _registry.TryGetValue(key, out value);
     }
--- src/Polly.Shared/Registry/IPolicyRegistry.cs
+++ src/Polly.Shared/Registry/IPolicyRegistry.cs
@@ -4,65 +4,65 @@ using System.Collections.Generic;
 namespace Polly.Registry
 {
     /// <summary>
-    /// Represents a collection of <see cref="Polly.Policy"/> keyed by <typeparamref name="Key"/>
+    /// Represents a collection of <see cref="Polly.Policy"/> keyed by <typeparamref name="Key"/>.
     /// </summary>
-    /// <typeparam name="Key">The type of keys in the dictionary</typeparam>
-    /// <typeparam name="Policy">The type of Policy to store. Must have <see cref="Polly.Policy"/> as base class. </typeparam>
+    /// <typeparam name="Key">The type of keys in the dictionary.</typeparam>
+    /// <typeparam name="Policy">The type of Policy to store. Must be derived from <see cref="Polly.Policy"/>.</typeparam>
     public interface IPolicyRegistry<Key, Policy> where Policy: Polly.Policy
     {
         /// <summary>
-        /// Gets or sets policy with specified Key.
+        /// Gets or sets <typeparamref name="Policy"/> with specified Key.
         /// </summary>
         /// <param name="key">The key of the policy to get or set.</param>
         /// <returns>The policy with specified Key.</returns>
-        /// <exception cref="System.ArgumentNullException">Key is null.</exception>
-        /// <exception cref="KeyNotFoundException">The property is retrieved and key is not found.</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="key"/> is null.</exception>
+        /// <exception cref="KeyNotFoundException">Policy with the specified <paramref name="key"/> is not found in the registry.</exception>
         Policy this[Key key] { get; set; }
 
         /// <summary>
-        /// Total number of policies in the registry
+        /// Total number of policies in the registry.
         /// </summary>
         int Count { get; }
 
         /// <summary>
-        /// Adds an element with the provided key and <see cref="Polly.Policy"/> to the registry.
+        /// Adds a <typeparamref name="Policy"/> with the provided key to the registry.
         /// </summary>
-        /// <param name="key">The key of the element to add.</param>
-        /// <param name="value">The <see cref="Polly.Policy"/> to store in the registry.</param>
-        /// <exception cref="ArgumentNullException"><paramref name="key"/> is null</exception>
-        /// <exception cref="ArgumentException">A Policy with same <paramref name="key"/> already exists</exception>
+        /// <param name="key">The key of the <typeparamref name="Policy"/> to add.</param>
+        /// <param name="value">The <typeparamref name="Policy"/> to store in the registry.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="key"/> is null.</exception>
+        /// <exception cref="ArgumentException">A Policy with same <paramref name="key"/> already exists.</exception>
         void Add(Key key, Policy value);
 
         /// <summary>
-        /// Determines whether the specified Key exists.
+        /// Determines whether the specified <paramref name="key"/> exists.
         /// </summary>
         /// <param name="key">The Key to locate in the registry</param>
-        /// <returns>True if Key exists otherwise false</returns>
+        /// <returns>True if <paramref name="key"/> exists otherwise false</returns>
         /// <exception cref="ArgumentNullException"><paramref name="key"/> is null</exception>
         bool ContainsKey(Key key);
 
         /// <summary>
-        /// Removes the specified <see cref="Polly.Policy"/> from the registry
+        /// Removes the specified <typeparamref name="Policy"/> from the registry.
         /// </summary>
-        /// <param name="key">The key of the policy to remove</param>
-        /// <returns>True if Policy is successfully removed. Otherwise false.</returns>
-        /// <exception cref="ArgumentNullException">key is null.</exception>
+        /// <param name="key">The key of the policy to remove.</param>
+        /// <returns>True if <see cref="Polly.Policy"/> is successfully removed. Otherwise false.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="key"/> is null.</exception>
         bool Remove(Key key);
 
         /// <summary>
-        /// Gets the Policy associated with the specified key
+        /// Gets the <typeparamref name="Policy"/> associated with the specified key.
         /// </summary>
-        /// <param name="key">The key whose value to get</param>
+        /// <param name="key">The key whose value to get.</param>
         /// <param name="value">
-        /// When this method returns, the Policy associated with the specified key, if the
-        /// key is found; otherwise, the default value for the type of the value parameter.
+        /// This method returns the <typeparamref name="Policy"/> associated with the specified <paramref name="key"/>, if the
+        /// key is found; otherwise null.
         /// This parameter is passed uninitialized.
         /// </param>
-        /// <returns>True if Policy exists for the provided Key. False otherwise</returns>
+        /// <returns>True if Policy exists for the provided Key. False otherwise.</returns>
         bool TryGetValue(Key key, out Policy value);
 
         /// <summary>
-        /// Removes all keys and values from registry
+        /// Removes all keys and policies from registry.
         /// </summary>
         void Clear();
     }
