0ce054f8f5b79b6dcd5702bb2e210d7d657bd12a
reisenberger <software@reisenberger.net>
2017-11-03 17:37:34 +0000

    Add v5.5.0 doco

--- src/Polly.NetStandard11/Properties/AssemblyInfo.cs
+++ src/Polly.NetStandard11/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("Polly")]
-[assembly: AssemblyVersion("5.4.0.0")]
+[assembly: AssemblyVersion("5.5.0.0")]
 [assembly: CLSCompliant(true)]
 
 [assembly: InternalsVisibleTo("Polly.NetStandard11.Specs")]
