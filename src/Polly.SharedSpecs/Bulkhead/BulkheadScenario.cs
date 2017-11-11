4bc758c3f4e40bc0502a98eb885af52164a7f251
reisenberger <software@reisenberger.net>
2017-11-11 16:59:56 +0000

    Bulkhead test scenarios - presentation improvement
    Make meaning of bulkhead test scenarios clearer by constructing struct
    with strongly-typed and named parameters  (work round xUnit object[]
    syntax)

---
+++ src/Polly.SharedSpecs/Bulkhead/BulkheadScenario.cs
@@ -0,0 +1,27 @@
+﻿namespace Polly.Specs.Bulkhead
+{
+    internal struct BulkheadScenario
+    {
+        readonly int _maxParallelization;
+        readonly int _maxQueuingActions;
+        readonly int _totalTestLoad;
+        readonly string _scenario;
+        readonly bool _cancelQueuing;
+        readonly bool _cancelExecuting;
+
+        public BulkheadScenario(int maxParallelization, int maxQueuingActions, int totalTestLoad, bool cancelQueuing, bool cancelExecuting, string scenario)
+        {
+             _maxParallelization = maxParallelization;
+             _maxQueuingActions = maxQueuingActions;
+             _totalTestLoad = totalTestLoad;
+             _scenario = scenario;
+             _cancelQueuing = cancelQueuing;
+             _cancelExecuting = cancelExecuting;
+        }
+
+        public object[] ToTheoryData()
+        {
+            return new object[] {_maxParallelization, _maxQueuingActions, _totalTestLoad, _cancelQueuing, _cancelExecuting, _scenario };
+        }
+    }
+}
