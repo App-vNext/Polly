7cefc4c9c1f995a0a6032f655ff0730d0bf85c15
Ben Arnold <ben@arnoldfamily.me.uk>
2018-01-31 17:19:23 +0000

    RelativeTtl is relative to the time the TTL is requested
    Fixes #403: RelativeTtl now always returns a Ttl relative from the time the item is put in the cache

--- src/Polly.Shared/Caching/RelativeTtl.cs
+++ src/Polly.Shared/Caching/RelativeTtl.cs
@@ -1,30 +1,34 @@
 ﻿using System;
-using Polly.Utilities;
 
 namespace Polly.Caching
 {
     /// <summary>
-    /// Defines a ttl strategy which will cache items until the specified point-in-time.
+    /// Defines a ttl strategy which will cache items for the specified time.
     /// </summary>
-    public class RelativeTtl : NonSlidingTtl
+    public class RelativeTtl : ITtlStrategy
     {
-        private static readonly TimeSpan DateTimeOffSetMaxTimeSpan = DateTimeOffset.MaxValue.Subtract(DateTimeOffset.MinValue);
+        private readonly TimeSpan ttl;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="RelativeTtl"/> class.
         /// </summary>
         /// <param name="ttl">The timespan for which to consider the cache item valid.</param>
-        public RelativeTtl(TimeSpan ttl) : base(
-            ttl < TimeSpan.Zero ? throw new ArgumentOutOfRangeException(nameof(ttl), "The ttl for items to cache must be greater than zero.")
-            : 
-            ttl == TimeSpan.MaxValue ? DateTimeOffset.MaxValue
-            : 
-            ttl >= DateTimeOffSetMaxTimeSpan ? DateTimeOffset.MaxValue 
-            : 
-            SystemClock.DateTimeOffsetUtcNow() > DateTimeOffset.MaxValue.Subtract(ttl) ? DateTimeOffset.MaxValue  
-            :
-            SystemClock.DateTimeOffsetUtcNow().Add(ttl))
+        public RelativeTtl(TimeSpan ttl)
         {
+            if (ttl < TimeSpan.Zero) throw new ArgumentOutOfRangeException(nameof(ttl), "The ttl for items to cache must be greater than zero.");
+
+            this.ttl = ttl;
+        }
+
+        /// <summary>
+        /// Gets a TTL for a cacheable item, given the current execution context.
+        /// </summary>
+        /// <param name="context">The execution context.</param>
+        /// <param name="result">The execution result.</param>
+        /// <returns>A <see cref="Ttl"/> representing the remaining Ttl of the cached item.</returns>
+        public Ttl GetTtl(Context context, object result)
+        {
+            return new Ttl(ttl);
         }
     }
 }
