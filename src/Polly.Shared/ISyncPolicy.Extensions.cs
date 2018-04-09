bf90b75e89d9479a617e90ee5258ead623ee729a
reisenberger <software@reisenberger.net>
2018-04-09 21:10:44 +0100

    Add .AsPolicy<TResult> and .AsAsyncPolicy<TResult>
    Add .AsPolicy&lt;TResult&gt; and .AsAsyncPolicy&lt;TResult&gt; methods for converting non-generic policies to generic policies.

---
+++ src/Polly.Shared/ISyncPolicy.Extensions.cs
@@ -0,0 +1,20 @@
+﻿namespace Polly
+{
+    /// <summary>
+    /// Contains extensions methods on <see cref="ISyncPolicy"/>
+    /// </summary>
+    public static class ISyncPolicyExtensions
+    {
+        /// <summary>
+        /// Converts a non-generic <see cref="ISyncPolicy"/> into a generic <see cref="ISyncPolicy{TResult}"/> for handling only executions returning <typeparamref name="TResult"/>.
+        /// </summary>
+        /// <remarks>This method allows you to convert a non-generic <see cref="ISyncPolicy"/> into a generic <see cref="ISyncPolicy{TResult}"/> for contexts such as variables or parameters which may explicitly require a generic <see cref="ISyncPolicy{TResult}"/>. </remarks>
+        /// <param name="policy">The non-generic <see cref="ISyncPolicy"/>  to convert to a generic <see cref="ISyncPolicy{TResult}"/>.</param>
+        /// <returns>A generic <see cref="ISyncPolicy{TResult}"/> version of the supplied non-generic <see cref="ISyncPolicy"/>.</returns>
+        public static ISyncPolicy<TResult> AsPolicy<TResult>(this ISyncPolicy policy)
+        {
+            return policy.Wrap(Policy.NoOp<TResult>());
+        }
+    }
+
+}
