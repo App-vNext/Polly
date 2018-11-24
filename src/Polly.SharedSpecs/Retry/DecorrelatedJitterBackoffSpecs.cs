c7f56c212dbdc2d4cc87172feda321eea6ec8fc5
reisenberger <software@reisenberger.net>
2018-11-24 12:17:51 +0000

    Demonstrate randomising IEnumerable can be reused
    Add test demonstrating that the IEnumerable returned from DecorrelatedJitterBackoff.Generate(...) can be reused multiple times, and generates different jitter each time.
    
    Fix other specs to assert on only a single reified sequence from the enumerable.

--- src/Polly.SharedSpecs/Retry/DecorrelatedJitterBackoffSpecs.cs
+++ src/Polly.SharedSpecs/Retry/DecorrelatedJitterBackoffSpecs.cs
@@ -22,8 +22,8 @@ namespace Polly.Specs.Retry
 
             DecorrelatedJitterBackoff backoff1 = new DecorrelatedJitterBackoff(minDelay, maxDelay, fastFirst, Seed);
             DecorrelatedJitterBackoff backoff2 = new DecorrelatedJitterBackoff(minDelay, maxDelay, fastFirst, Seed);
-            IEnumerable<TimeSpan> discrete1 = backoff1.Generate(count);
-            IEnumerable<TimeSpan> discrete2 = backoff2.Generate(count);
+            IEnumerable<TimeSpan> discrete1 = backoff1.Generate(count).ToList();
+            IEnumerable<TimeSpan> discrete2 = backoff2.Generate(count).ToList();
 
             discrete1.Should().HaveCount(count);
             discrete2.Should().HaveCount(count);
@@ -41,8 +41,8 @@ namespace Polly.Specs.Retry
 
             DecorrelatedJitterBackoff backoff1 = new DecorrelatedJitterBackoff(minDelay, maxDelay, fastFirst, 123);
             DecorrelatedJitterBackoff backoff2 = new DecorrelatedJitterBackoff(minDelay, maxDelay, fastFirst, 321);
-            IEnumerable<TimeSpan> discrete1 = backoff1.Generate(count);
-            IEnumerable<TimeSpan> discrete2 = backoff2.Generate(count);
+            IEnumerable<TimeSpan> discrete1 = backoff1.Generate(count).ToList();
+            IEnumerable<TimeSpan> discrete2 = backoff2.Generate(count).ToList();
 
             discrete1.Should().HaveCount(count);
             discrete2.Should().HaveCount(count);
@@ -66,8 +66,8 @@ namespace Polly.Specs.Retry
 
             DecorrelatedJitterBackoff backoff1 = new DecorrelatedJitterBackoff(minDelay, maxDelay, fastFirst);
             DecorrelatedJitterBackoff backoff2 = new DecorrelatedJitterBackoff(minDelay, maxDelay, fastFirst);
-            IEnumerable<TimeSpan> discrete1 = backoff1.Generate(count);
-            IEnumerable<TimeSpan> discrete2 = backoff2.Generate(count);
+            IEnumerable<TimeSpan> discrete1 = backoff1.Generate(count).ToList();
+            IEnumerable<TimeSpan> discrete2 = backoff2.Generate(count).ToList();
 
             discrete1.Should().HaveCount(count);
             discrete2.Should().HaveCount(count);
@@ -90,7 +90,7 @@ namespace Polly.Specs.Retry
             TimeSpan maxDelay = TimeSpan.FromMilliseconds(1500);
 
             DecorrelatedJitterBackoff backoff = new DecorrelatedJitterBackoff(minDelay, maxDelay, fastFirst, Seed);
-            IEnumerable<TimeSpan> discrete = backoff.Generate(count);
+            IEnumerable<TimeSpan> discrete = backoff.Generate(count).ToList();
 
             discrete.Should().HaveCount(count);
 
@@ -120,7 +120,7 @@ namespace Polly.Specs.Retry
             TimeSpan maxDelay = TimeSpan.FromMilliseconds(150_000);
 
             DecorrelatedJitterBackoff backoff = new DecorrelatedJitterBackoff(minDelay, maxDelay, fastFirst, Seed);
-            IEnumerable<TimeSpan> discrete = backoff.Generate(count);
+            IEnumerable<TimeSpan> discrete = backoff.Generate(count).ToList();
 
             discrete.Should().HaveCount(count);
 
@@ -150,7 +150,7 @@ namespace Polly.Specs.Retry
             TimeSpan maxDelay = TimeSpan.FromMilliseconds(0);
 
             DecorrelatedJitterBackoff backoff = new DecorrelatedJitterBackoff(minDelay, maxDelay, fastFirst, Seed);
-            IEnumerable<TimeSpan> discrete = backoff.Generate(count);
+            IEnumerable<TimeSpan> discrete = backoff.Generate(count).ToList();
 
             discrete.Should().HaveCount(count);
 
@@ -173,7 +173,7 @@ namespace Polly.Specs.Retry
             TimeSpan maxDelay = TimeSpan.FromMilliseconds(1500);
 
             DecorrelatedJitterBackoff backoff = new DecorrelatedJitterBackoff(minDelay, maxDelay, fastFirst, Seed);
-            IEnumerable<TimeSpan> discrete = backoff.Generate(count);
+            IEnumerable<TimeSpan> discrete = backoff.Generate(count).ToList();
 
             discrete.Should().BeEmpty();
         }
@@ -184,7 +184,7 @@ namespace Polly.Specs.Retry
             const int count = 20;
             DecorrelatedJitterBackoff durationStrategy = new DecorrelatedJitterBackoff(TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(3), false);
 
-            IEnumerable<TimeSpan> actualDurations = durationStrategy.Generate(count);
+            IEnumerable<TimeSpan> actualDurations = durationStrategy.Generate(count).ToList();
             actualDurations.Should().OnlyContain(n => n >= durationStrategy.MinDelay && n <= durationStrategy.MaxDelay);
         }
 
@@ -194,9 +194,33 @@ namespace Polly.Specs.Retry
             const int count = 20;
             DecorrelatedJitterBackoff durationStrategy = new DecorrelatedJitterBackoff(TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(3), true);
 
-            IEnumerable<TimeSpan> actualDurations = durationStrategy.Generate(count);
+            IEnumerable<TimeSpan> actualDurations = durationStrategy.Generate(count).ToList();
             actualDurations.Take(1).Should().OnlyContain(n => n == TimeSpan.Zero);
             actualDurations.Skip(1).Should().OnlyContain(n => n >= durationStrategy.MinDelay && n <= durationStrategy.MaxDelay);
+        }
+
+        [Fact]
+        public static void A_single_instance_of_IEnumerable_returned_from_Generate_method_should_generate_different_results_each_time_it_is_enumerated()
+        {
+            const int count = 20;
+            DecorrelatedJitterBackoff durationStrategy = new DecorrelatedJitterBackoff(TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(3));
+
+            // Take an instance of IEnumerable<TimeSpan> as directly returned by DecorrelatedJitterBackoff.Generate(...), exactly as users should when configuring a policy.
+            IEnumerable<TimeSpan> generate = durationStrategy.Generate(count);
+
+            // Check: We definitely only have a single instance of - we are repeatedly using the same instance of - IEnumerable<TimeSpan>
+            generate.Should().BeSameAs(generate); // BeSameAs(...) in FluentAssertions means refer to exact same instance in memory.
+
+            // Check: If enumerate that same instance twice, we get _different_ reified enumerated sequences.
+            generate.SequenceEqual(generate).Should().BeFalse(); // Checks the reified sequences generated by enumerating twice. 
+
+            // A more explicit version of the preceding assertion, for clarity:
+            generate.ToList().SequenceEqual(generate.ToList()).Should().BeFalse(); // Twice, reifies the sequence into a list; the two generated lists differ.
+
+            // But as the below statements show, users should not reify the list before passing the IEnumerable to the retry policy.  
+            // That would indeed cause the same sequence of timespans to be reused every time the policy was used.
+            IEnumerable<TimeSpan> reifiedList = generate.ToList();
+            reifiedList.SequenceEqual(reifiedList).Should().BeTrue();
         }
     }
 }
