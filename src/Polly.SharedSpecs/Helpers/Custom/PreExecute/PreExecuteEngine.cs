14c0cde451838f20bc2642cad555af2c2f3eea09
reisenberger <software@reisenberger.net>
2018-12-29 21:02:38 +0000

    Add simple examples of active and reactive custom policies, and tests

---
+++ src/Polly.SharedSpecs/Helpers/Custom/PreExecute/PreExecuteEngine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace Polly.Specs.Helpers.Custom.PreExecute
+{
+    internal static class PreExecuteEngine
+    {
+        internal static void Implementation(
+            Action preExecute,
+            Action<Context, CancellationToken> action, 
+            Context context, 
+            CancellationToken cancellationToken)
+        {
+            preExecute?.Invoke();
+
+            action(context, cancellationToken);
+        }
+
+        internal static TResult Implementation<TResult>(
+            Action preExecute,
+            Func<Context, CancellationToken, TResult> action,
+            Context context,
+            CancellationToken cancellationToken)
+        {
+            preExecute?.Invoke();
+
+            return action(context, cancellationToken);
+        }
+    }
+}
