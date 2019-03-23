7d70287913e635ac34779f7a7194e65e835f2b69
reisenberger <software@reisenberger.net>
2019-03-23 20:19:28 +0000

    Add example async TResult rate-limiter policy implementation

---
+++ src/Polly/RateLimit/AsyncRateLimitEngine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Polly.RateLimit
+{
+    internal static class AsyncRateLimitEngine
+    {
+        internal static async Task<TResult> ImplementationAsync<TResult>(
+            IRateLimiter rateLimiter,
+            Func<Context, TResult> retryAfterFactory,
+            Func<Context, CancellationToken, Task<TResult>> action,
+            Context context, CancellationToken cancellationToken, bool continueOnCapturedContext
+            )
+        {
+            (bool permit, TimeSpan retryAfter) = rateLimiter.PermitExecution();
+
+            if (permit)
+            {
+                return await action(context, cancellationToken).ConfigureAwait(continueOnCapturedContext);
+            }
+
+            if (retryAfterFactory != null)
+            {
+                return retryAfterFactory(context);
+            }
+
+            throw new RateLimitRejectedException(retryAfter);
+        }
+    }
+}
