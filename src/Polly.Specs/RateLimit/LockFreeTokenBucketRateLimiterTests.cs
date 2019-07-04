5496e66cabfa0a945fafcb8481c3c3dde6f83144
reisenberger <software@reisenberger.net>
2019-07-04 06:29:41 +0100

    Factor out test helpers

--- src/Polly.Specs/RateLimit/LockFreeTokenBucketRateLimiterTests.cs
+++ src/Polly.Specs/RateLimit/LockFreeTokenBucketRateLimiterTests.cs
@@ -7,6 +7,7 @@ using FluentAssertions;
 using Polly.RateLimit;
 using Polly.Utilities;
 using FluentAssertions.Execution;
+using Polly.Specs.Helpers.RateLimit;
 using Xunit;
 using Xunit.Sdk;
 
@@ -32,22 +33,14 @@ namespace Polly.Specs.RateLimit
             TimeSpan onePer = TimeSpan.FromSeconds(onePerSeconds);
             var rateLimiter = GetRateLimiter(onePer, 1);
 
-            // Act - first execution after initialising should always be permitted.
-            (bool permitExecution, TimeSpan retryAfter) canExecute = rateLimiter.PermitExecution();
-
-            // Assert.
-            canExecute.permitExecution.Should().BeTrue();
-            canExecute.retryAfter.Should().Be(TimeSpan.Zero);
+            // Assert - first execution after initialising should always be permitted.
+            rateLimiter.ShouldPermitAnExecution();
 
             // Arrange
             // (do nothing - time not advanced)
 
-            // Act - try another execution
-            canExecute = rateLimiter.PermitExecution();
-
             // Assert - should be blocked - time not advanced.
-            canExecute.permitExecution.Should().BeFalse();
-            canExecute.retryAfter.Should().Be(onePer);
+            rateLimiter.ShouldNotPermitAnExecution();
         }
 
         [Theory]
@@ -63,16 +56,10 @@ namespace Polly.Specs.RateLimit
             var rateLimiter = GetRateLimiter(onePer, bucketCapacity);
 
             // Act - should be able to successfully take bucketCapacity items.
-            AssertCanTakeNItems(rateLimiter, bucketCapacity);
+            rateLimiter.ShouldPermitNExecutions(bucketCapacity);
 
-            // After that, should not be able to take any items (given time not advanced).
-
-            // Act
-            (bool permitExecution, TimeSpan retryAfter) canExecute = rateLimiter.PermitExecution();
-
-            // Assert - should be blocked - time not advanced.
-            canExecute.permitExecution.Should().BeFalse();
-            canExecute.retryAfter.Should().BeGreaterThan(TimeSpan.Zero);
+            // Assert - should not be able to take any items (given time not advanced).
+            rateLimiter.ShouldNotPermitAnExecution();
         }
 
 
@@ -85,43 +72,35 @@ namespace Polly.Specs.RateLimit
         [InlineData(5, 10)]
         public void Given_any_bucket_capacity_ratelimiter_permits_another_execution_per_interval(int onePerSeconds, int bucketCapacity)
         {
+            FixClock();
+
             // Arrange
             TimeSpan onePer = TimeSpan.FromSeconds(onePerSeconds);
             var rateLimiter = GetRateLimiter(onePer, bucketCapacity);
 
-            FixClock();
-
             // Arrange - spend the initial bucket capacity.
-            AssertCanTakeNItems(rateLimiter, bucketCapacity);
+            rateLimiter.ShouldPermitNExecutions(bucketCapacity);
 
             // Act-Assert - repeatedly advance the clock towards the interval but not quite - then to the interval
             int experimentRepeats = bucketCapacity * 3;
-            const int shortfallFromInterval = 1;
-            long notQuiteInterval = onePer.Ticks - shortfallFromInterval;
-            (bool permitExecution, TimeSpan retryAfter) canExecute;
+            TimeSpan shortfallFromInterval = TimeSpan.FromTicks(1);
+            TimeSpan notQuiteInterval = onePer - shortfallFromInterval;
             for (int i = 0; i < experimentRepeats; i++)
             {
                 // Arrange - Advance clock not quite to the interval
-                AdvanceClock(notQuiteInterval);
-                // Act
-                canExecute = rateLimiter.PermitExecution();
+                AdvanceClock(notQuiteInterval.Ticks);
+
                 // Assert - should not quite be able to issue another token
-                canExecute.permitExecution.Should().BeFalse();
-                canExecute.retryAfter.Should().Be(TimeSpan.FromTicks(shortfallFromInterval));
+                rateLimiter.ShouldNotPermitAnExecution(shortfallFromInterval);
 
                 // Arrange - Advance clock to the interval
-                AdvanceClock(shortfallFromInterval);
-                // Act
-                canExecute = rateLimiter.PermitExecution();
-                // Assert - should be able to issue another token
-                canExecute.permitExecution.Should().BeTrue();
-                canExecute.retryAfter.Should().Be(TimeSpan.Zero);
+                AdvanceClock(shortfallFromInterval.Ticks);
 
                 // Act
-                canExecute = rateLimiter.PermitExecution();
-                // Assert - cannot get another token straight away
-                canExecute.permitExecution.Should().BeFalse();
-                canExecute.retryAfter.Should().BeGreaterThan(TimeSpan.Zero);
+                rateLimiter.ShouldPermitAnExecution();
+
+                // Assert - but cannot get another token straight away
+                rateLimiter.ShouldNotPermitAnExecution();
             }
         }
 
@@ -131,6 +110,8 @@ namespace Polly.Specs.RateLimit
         [InlineData(100)]
         public void Given_immediate_parallel_contention_ratelimiter_still_only_permits_one(int parallelContention)
         {
+            FixClock();
+
             // Arrange
             TimeSpan onePer = TimeSpan.FromSeconds(1);
             var rateLimiter = GetRateLimiter(onePer, 1);
@@ -195,20 +176,6 @@ namespace Polly.Specs.RateLimit
         {
             DateTimeOffset now = SystemClock.DateTimeOffsetUtcNow();
             SystemClock.DateTimeOffsetUtcNow = () => now + TimeSpan.FromTicks(advanceTicks);
-        }
-
-        private static void AssertCanTakeNItems(IRateLimiter rateLimiter, long expectedTake)
-        {
-            (bool permitExecution, TimeSpan retryAfter) canExecute;
-            for (int take = 0; take < expectedTake; take++)
-            {
-                // Act
-                canExecute = rateLimiter.PermitExecution();
-
-                // Assert.
-                canExecute.permitExecution.Should().BeTrue();
-                canExecute.retryAfter.Should().Be(TimeSpan.Zero);
-            }
         }
 
         public void Dispose()
