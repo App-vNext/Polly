23f26e8e48dd873c90f6bea51301ab86e4e696e4
reisenberger <software@reisenberger.net>
2019-07-04 22:30:43 +0100

    Add first specs on async policy syntax

---
+++ src/Polly.Specs/RateLimit/AsyncRateLimitPolicyTResultSpecs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Polly.RateLimit;
+using Polly.Specs.Helpers;
+using Polly.Utilities;
+using Xunit;
+
+namespace Polly.Specs.RateLimit
+{
+    [Collection(Polly.Specs.Helpers.Constants.SystemClockDependentTestCollection)]
+    public class AsyncRateLimitPolicyTResultSpecs : RateLimitPolicySpecsBase, IDisposable
+    {
+        public void Dispose()
+        {
+            SystemClock.Reset();
+        }
+
+        public override IRateLimitPolicy GetPolicyViaSyntax(int numberOfExecutions, TimeSpan perTimeSpan)
+        {
+            return Policy.RateLimitAsync<ResultClass>(numberOfExecutions, perTimeSpan);
+        }
+
+        public override IRateLimitPolicy GetPolicyViaSyntax(int numberOfExecutions, TimeSpan perTimeSpan, int maxBurst)
+        {
+            return Policy.RateLimitAsync<ResultClass>(numberOfExecutions, perTimeSpan, maxBurst);
+        }
+    }
+}
---
+++ src/Polly.Specs/RateLimit/RateLimitPolicySpecsBase.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using FluentAssertions;
+using Polly.RateLimit;
+using Xunit;
+
+namespace Polly.Specs.RateLimit
+{
+    public abstract class RateLimitPolicySpecsBase
+    {
+        public abstract IRateLimitPolicy GetPolicyViaSyntax(
+            int numberOfExecutions,
+            TimeSpan perTimeSpan);
+
+        public abstract IRateLimitPolicy GetPolicyViaSyntax(
+            int numberOfExecutions,
+            TimeSpan perTimeSpan,
+            int maxBurst);
+
+        [Fact]
+        public void Syntax_should_throw_for_numberOfExecutions_negative()
+        {
+            Action invalidSyntax = () => GetPolicyViaSyntax(-1, TimeSpan.FromSeconds(1));
+
+            invalidSyntax.ShouldThrow<ArgumentOutOfRangeException>().And.ParamName.Should().Be("numberOfExecutions");
+        }
+
+        [Fact]
+        public void Syntax_should_throw_for_numberOfExecutions_zero()
+        {
+            Action invalidSyntax = () => GetPolicyViaSyntax(0, TimeSpan.FromSeconds(1));
+
+            invalidSyntax.ShouldThrow<ArgumentOutOfRangeException>().And.ParamName.Should().Be("numberOfExecutions");
+        }
+
+        [Fact]
+        public void Syntax_should_throw_for_perTimeSpan_negative()
+        {
+            Action invalidSyntax = () => GetPolicyViaSyntax(1, TimeSpan.FromTicks(-1));
+
+            invalidSyntax.ShouldThrow<ArgumentOutOfRangeException>().And.ParamName.Should().Be("perTimeSpan");
+        }
+
+        [Fact]
+        public void Syntax_should_throw_for_perTimeSpan_zero()
+        {
+            Action invalidSyntax = () => GetPolicyViaSyntax(1, TimeSpan.Zero);
+
+            invalidSyntax.ShouldThrow<ArgumentOutOfRangeException>().And.ParamName.Should().Be("perTimeSpan");
+        }
+
+        [Fact]
+        public void Syntax_should_throw_for_maxBurst_negative()
+        {
+            Action invalidSyntax = () => GetPolicyViaSyntax(1, TimeSpan.FromSeconds(1), -1);
+
+            invalidSyntax.ShouldThrow<ArgumentOutOfRangeException>().And.ParamName.Should().Be("maxBurst");
+        }
+
+        [Fact]
+        public void Syntax_should_throw_for_maxBurst_zero()
+        {
+            Action invalidSyntax = () => GetPolicyViaSyntax(1, TimeSpan.FromSeconds(1), 0);
+
+            invalidSyntax.ShouldThrow<ArgumentOutOfRangeException>().And.ParamName.Should().Be("maxBurst");
+        }
+    }
+}
