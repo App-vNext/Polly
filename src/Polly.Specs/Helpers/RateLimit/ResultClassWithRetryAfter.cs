705cda98ba4faab3b9e84571d1b115f47a489feb
reisenberger <software@reisenberger.net>
2019-07-05 08:42:54 +0100

    Add tests on retryAfterFactory

---
+++ src/Polly.Specs/Helpers/RateLimit/ResultClassWithRetryAfter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Polly.Specs.Helpers.RateLimit
+{
+    internal class ResultClassWithRetryAfter : ResultClass
+    {
+        public TimeSpan RetryAfter { get; }
+
+        public ResultClassWithRetryAfter(ResultPrimitive result)
+        : base(result)
+        {
+            RetryAfter = TimeSpan.Zero;
+        }
+
+        public ResultClassWithRetryAfter(TimeSpan retryAfter)
+        : base(ResultPrimitive.Undefined)
+        {
+            RetryAfter = retryAfter;
+        }
+    }
+}
