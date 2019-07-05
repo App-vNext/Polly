d0f191bb0620700c4cd368ee302617ab71cc29c8
reisenberger <software@reisenberger.net>
2019-07-05 08:17:20 +0100

    Add full set of specs on rate-limit policies thus far

--- src/Polly.Specs/RateLimit/AsyncRateLimitPolicyTResultSpecs.cs
+++ src/Polly.Specs/RateLimit/AsyncRateLimitPolicyTResultSpecs.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Polly.RateLimit;
 using Polly.Specs.Helpers;
 using Polly.Utilities;
@@ -14,14 +15,34 @@ namespace Polly.Specs.RateLimit
             SystemClock.Reset();
         }
 
-        public override IRateLimitPolicy GetPolicyViaSyntax(int numberOfExecutions, TimeSpan perTimeSpan)
+        protected override IRateLimitPolicy GetPolicyViaSyntax(int numberOfExecutions, TimeSpan perTimeSpan)
         {
             return Policy.RateLimitAsync<ResultClass>(numberOfExecutions, perTimeSpan);
         }
 
-        public override IRateLimitPolicy GetPolicyViaSyntax(int numberOfExecutions, TimeSpan perTimeSpan, int maxBurst)
+        protected override IRateLimitPolicy GetPolicyViaSyntax(int numberOfExecutions, TimeSpan perTimeSpan, int maxBurst)
         {
             return Policy.RateLimitAsync<ResultClass>(numberOfExecutions, perTimeSpan, maxBurst);
+        }
+
+        protected override (bool, TimeSpan) TryExecuteThroughPolicy(IRateLimitPolicy policy)
+        {
+            if (policy is AsyncRateLimitPolicy<ResultClass> typedPolicy)
+            {
+                try
+                {
+                    typedPolicy.ExecuteAsync(() => Task.FromResult(new ResultClass(ResultPrimitive.Good))).GetAwaiter().GetResult();
+                    return (true, TimeSpan.Zero);
+                }
+                catch (RateLimitRejectedException e)
+                {
+                    return (false, e.RetryAfter);
+                }
+            }
+            else
+            {
+                throw new InvalidOperationException("Unexpected policy type in test construction.");
+            }
         }
     }
 }
