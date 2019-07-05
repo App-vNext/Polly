a734da2948a3cde428fe0802c58e339865cfd611
reisenberger <software@reisenberger.net>
2019-07-05 21:12:07 +0100

    Add sync rate-limit policies

---
+++ src/Polly/RateLimit/RateLimitEngine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace Polly.RateLimit
+{
+    internal static class RateLimitEngine
+    {
+        internal static TResult Implementation<TResult>(
+            IRateLimiter rateLimiter,
+            Func<TimeSpan, Context, TResult> retryAfterFactory,
+            Func<Context, CancellationToken, TResult> action,
+            Context context,
+            CancellationToken cancellationToken
+        )
+        {
+            (bool permit, TimeSpan retryAfter) = rateLimiter.PermitExecution();
+
+            if (permit)
+            {
+                return action(context, cancellationToken);
+            }
+
+            if (retryAfterFactory != null)
+            {
+                return retryAfterFactory(retryAfter, context);
+            }
+
+            throw new RateLimitRejectedException(retryAfter);
+        }
+    }
+}
