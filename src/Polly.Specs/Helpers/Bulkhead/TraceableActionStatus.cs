e4353bf4446cd3aaa7b23c57fb7732beb33f6c7b
reisenberger <software@reisenberger.net>
2019-11-25 08:44:50 +0000

    Extensively refactor bulkhead tests

---
+++ src/Polly.Specs/Helpers/Bulkhead/TraceableActionStatus.cs
@@ -0,0 +1,17 @@
+﻿namespace Polly.Specs.Helpers.Bulkhead
+{
+    /// <summary>
+    /// States of a <see cref="TraceableAction"/> that can be tracked during testing.
+    /// </summary>
+    public enum TraceableActionStatus
+    {
+        Unstarted,
+        StartRequested,
+        QueueingForSemaphore,
+        Executing,
+        Rejected,
+        Canceled,
+        Faulted,
+        Completed,
+    }
+}
