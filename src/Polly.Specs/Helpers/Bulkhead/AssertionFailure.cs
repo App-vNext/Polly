6383ab37d311a164295a16432fadeed67347a192
reisenberger <software@reisenberger.net>
2019-11-25 08:44:50 +0000

    Extensively refactor bulkhead tests

---
+++ src/Polly.Specs/Helpers/Bulkhead/AssertionFailure.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Polly.Specs.Helpers.Bulkhead
+{
+    public class AssertionFailure
+    {
+        public AssertionFailure(int expected, int actual, string measure)
+        {
+            if (string.IsNullOrWhiteSpace(measure)) throw new ArgumentNullException(nameof(measure));
+
+            Expected = expected;
+            Actual = actual;
+            Measure = measure;
+        }
+
+        public int Expected { get; }
+        public int Actual { get; }
+        public string Measure { get; }
+    }
+}
---
+++ src/Polly.Specs/Helpers/Bulkhead/SilentOutputHelper.cs
@@ -0,0 +1,17 @@
+﻿using Xunit.Abstractions;
+
+namespace Polly.Specs.Helpers.Bulkhead
+{
+    public class SilentOutputHelper : ITestOutputHelper
+    {
+        public void WriteLine(string message)
+        {
+            // Do nothing: intentionally silent.
+        }
+
+        public void WriteLine(string format, params object[] args)
+        {
+            // Do nothing: intentionally silent.
+        }
+    }
+}
