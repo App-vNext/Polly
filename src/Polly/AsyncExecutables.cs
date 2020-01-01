b77847c5355ea33defa8ede5638afb1d9a423649
reisenberger <software@reisenberger.net>
2020-01-01 14:14:32 +0000

    Add async struct execution despatch fundamentals

---
+++ src/Polly/AsyncExecutables.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Polly.Utilities;
+
+namespace Polly
+{
+    /// <inheritdoc/>
+    internal readonly struct AsyncExecutableActionNoParams : IAsyncExecutable<EmptyStruct>
+    {
+        private readonly Func<Task> _action;
+
+        public AsyncExecutableActionNoParams(Func<Task> action)
+        {
+            _action = action;
+        }
+
+        /// <inheritdoc/>
+        public async Task<EmptyStruct> ExecuteAsync(Context context, CancellationToken cancellationToken, bool continueOnCapturedContext)
+        {
+            await _action().ConfigureAwait(continueOnCapturedContext);
+            return EmptyStruct.Instance;
+        }
+    }
+
+    /// <inheritdoc/>
+    internal readonly struct AsyncExecutableActionOnContext : IAsyncExecutable<EmptyStruct>
+    {
+        private readonly Func<Context, Task> _action;
+
+        public AsyncExecutableActionOnContext(Func<Context, Task> action)
+        {
+            _action = action;
+        }
+
+        /// <inheritdoc/>
+        public async Task<EmptyStruct> ExecuteAsync(Context context, CancellationToken cancellationToken, bool continueOnCapturedContext)
+        {
+            await _action(context).ConfigureAwait(continueOnCapturedContext);
+            return EmptyStruct.Instance;
+        }
+    }
+
+    /// <inheritdoc/>
+    internal readonly struct AsyncExecutableActionOnCancellationToken : IAsyncExecutable<EmptyStruct>
+    {
+        private readonly Func<CancellationToken, Task> _action;
+
+        public AsyncExecutableActionOnCancellationToken(Func<CancellationToken, Task> action)
+        {
+            _action = action;
+        }
+
+        /// <inheritdoc/>
+        public async Task<EmptyStruct> ExecuteAsync(Context context, CancellationToken cancellationToken, bool continueOnCapturedContext)
+        {
+            await _action(cancellationToken).ConfigureAwait(continueOnCapturedContext);
+            return EmptyStruct.Instance;
+        }
+    }
+
+    /// <inheritdoc/>
+    internal readonly struct AsyncExecutableActionOnContextCancellationToken : IAsyncExecutable<EmptyStruct>
+    {
+        private readonly Func<Context, CancellationToken, Task> _action;
+
+        public AsyncExecutableActionOnContextCancellationToken(Func<Context, CancellationToken, Task> action)
+        {
+            _action = action;
+        }
+
+        /// <inheritdoc/>
+        public async Task<EmptyStruct> ExecuteAsync(Context context, CancellationToken cancellationToken, bool continueOnCapturedContext)
+        {
+            await _action(context, cancellationToken).ConfigureAwait(continueOnCapturedContext);
+            return EmptyStruct.Instance;
+        }
+    }
+
+    /// <inheritdoc/>
+    internal readonly struct AsyncExecutableAction : IAsyncExecutable<EmptyStruct>
+    {
+        private readonly Func<Context, CancellationToken, bool, Task> _action;
+
+        public AsyncExecutableAction(Func<Context, CancellationToken, bool, Task> action)
+        {
+            _action = action;
+        }
+
+        /// <inheritdoc/>
+        public async Task<EmptyStruct> ExecuteAsync(Context context, CancellationToken cancellationToken, bool continueOnCapturedContext)
+        {
+            await _action(context, cancellationToken, continueOnCapturedContext).ConfigureAwait(continueOnCapturedContext);
+            return EmptyStruct.Instance;
+        }
+    }
+
+    internal readonly struct AsyncExecutableAction<T1> : IAsyncExecutable<EmptyStruct>
+    {
+        private readonly Func<Context, CancellationToken, bool, T1, Task> _action;
+        private readonly T1 _arg1;
+
+        public AsyncExecutableAction(Func<Context, CancellationToken, bool, T1, Task> action, T1 arg1)
+        {
+            _action = action;
+            _arg1 = arg1;
+        }
+
+        public async Task<EmptyStruct> ExecuteAsync(Context context, CancellationToken cancellationToken, bool continueOnCapturedContext)
+        {
+            await _action(context, cancellationToken, continueOnCapturedContext, _arg1).ConfigureAwait(continueOnCapturedContext);
+            return EmptyStruct.Instance;
+        }
+    }
+
+    internal readonly struct AsyncExecutableAction<T1, T2> : IAsyncExecutable<EmptyStruct>
+    {
+        private readonly Func<Context, CancellationToken, bool, T1, T2, Task> _action;
+        private readonly T1 _arg1;
+        private readonly T2 _arg2;
+
+        public AsyncExecutableAction(Func<Context, CancellationToken, bool, T1, T2, Task> action, T1 arg1, T2 arg2)
+        {
+            _action = action;
+            _arg1 = arg1;
+            _arg2 = arg2;
+        }
+
+        public async Task<EmptyStruct> ExecuteAsync(Context context, CancellationToken cancellationToken, bool continueOnCapturedContext)
+        {
+            await _action(context, cancellationToken, continueOnCapturedContext, _arg1, _arg2).ConfigureAwait(continueOnCapturedContext);
+            return EmptyStruct.Instance;
+        }
+    }
+
+    /// <inheritdoc/>
+    internal readonly struct AsyncExecutableFuncNoParams<TResult> : IAsyncExecutable<TResult>
+    {
+        private readonly Func<Task<TResult>> _func;
+
+        /// <summary>
+        /// Creates a <see cref="AsyncExecutableFuncNoParams{TResult}"/> struct for the passed func, which may be executed through a policy at a later point in time.
+        /// </summary>
+        /// <param name="func">The function.</param>
+        public AsyncExecutableFuncNoParams(Func<Task<TResult>> func)
+        {
+            _func = func;
+        }
+
+        /// <inheritdoc/>
+        public Task<TResult> ExecuteAsync(Context context, CancellationToken cancellationToken, bool continueOnCapturedContext) => _func();
+    }
+
+    /// <inheritdoc/>
+    internal readonly struct AsyncExecutableFuncOnContext<TResult> : IAsyncExecutable<TResult>
+    {
+        private readonly Func<Context, Task<TResult>> _func;
+
+        /// <summary>
+        /// Creates a <see cref="AsyncExecutableFuncOnContext{TResult}"/> struct for the passed func, which may be executed through a policy at a later point in time.
+        /// </summary>
+        /// <param name="func">The function.</param>
+        public AsyncExecutableFuncOnContext(Func<Context, Task<TResult>> func)
+        {
+            _func = func;
+        }
+
+        /// <inheritdoc/>
+        public Task<TResult> ExecuteAsync(Context context, CancellationToken cancellationToken, bool continueOnCapturedContext) => _func(context);
+    }
+
+    /// <inheritdoc/>
+    internal readonly struct AsyncExecutableFuncOnCancellationToken<TResult> : IAsyncExecutable<TResult>
+    {
+        private readonly Func<CancellationToken, Task<TResult>> _func;
+
+        /// <summary>
+        /// Creates a <see cref="AsyncExecutableFuncOnCancellationToken{TResult}"/> struct for the passed func, which may be executed through a policy at a later point in time.
+        /// </summary>
+        /// <param name="func">The function.</param>
+        public AsyncExecutableFuncOnCancellationToken(Func<CancellationToken, Task<TResult>> func)
+        {
+            _func = func;
+        }
+
+        /// <inheritdoc/>
+        public Task<TResult> ExecuteAsync(Context context, CancellationToken cancellationToken, bool continueOnCapturedContext) => _func(cancellationToken);
+    }
+
+    /// <inheritdoc/>
+    internal readonly struct AsyncExecutableFuncOnContextCancellationToken<TResult> : IAsyncExecutable<TResult>
+    {
+        private readonly Func<Context, CancellationToken, Task<TResult>> _func;
+
+        /// <summary>
+        /// Creates a <see cref="AsyncExecutableFuncOnContextCancellationToken{TResult}"/> struct for the passed func, which may be executed through a policy at a later point in time.
+        /// </summary>
+        /// <param name="func">The function.</param>
+        public AsyncExecutableFuncOnContextCancellationToken(Func<Context, CancellationToken, Task<TResult>> func)
+        {
+            _func = func;
+        }
+
+        /// <inheritdoc/>
+        public Task<TResult> ExecuteAsync(Context context, CancellationToken cancellationToken, bool continueOnCapturedContext) => _func(context, cancellationToken);
+    }
+
+    /// <inheritdoc/>
+    internal readonly struct AsyncExecutableFunc<TResult> : IAsyncExecutable<TResult>
+    {
+        private readonly Func<Context, CancellationToken, bool, Task<TResult>> _func;
+
+        /// <summary>
+        /// Creates a <see cref="AsyncExecutableFunc{TResult}"/> struct for the passed func, which may be executed through a policy at a later point in time.
+        /// </summary>
+        /// <param name="func">The function.</param>
+        public AsyncExecutableFunc(Func<Context, CancellationToken, bool, Task<TResult>> func)
+        {
+            _func = func;
+        }
+
+        /// <inheritdoc/>
+        public Task<TResult> ExecuteAsync(Context context, CancellationToken cancellationToken, bool continueOnCapturedContext) => _func(context, cancellationToken, continueOnCapturedContext);
+    }
+
+    /// <inheritdoc/>
+    internal readonly struct AsyncExecutableFunc<T1, TResult> : IAsyncExecutable<TResult>
+    {
+        private readonly Func<Context, CancellationToken, bool, T1, Task<TResult>> _func;
+        private readonly T1 _arg1;
+
+        /// <summary>
+        /// Creates a <see cref="AsyncExecutableFunc{T1, TResult}"/> instance combining a func and an input parameter of type <typeparamref name="T1"/> to be used when executing it. 
+        /// This func may be executed through a policy at a later point in time.
+        /// <remarks>Combining the action and its input parameter into a short-lived struct, as shown here, allows users to pass data to actions executed through Polly policies without using closures, which would result in extra heap allocations.</remarks>
+        /// </summary>
+        /// <param name="func">The function.</param>
+        /// <param name="arg1">The parameter to pass, when executing the function.</param>
+        public AsyncExecutableFunc(Func<Context, CancellationToken, bool, T1, Task<TResult>> func, T1 arg1)
+        {
+            _func = func;
+            _arg1 = arg1;
+        }
+
+        /// <inheritdoc/>
+        public Task<TResult> ExecuteAsync(Context context, CancellationToken cancellationToken, bool continueOnCapturedContext) => _func(context, cancellationToken, continueOnCapturedContext, _arg1);
+    }
+
+    /// <inheritdoc/>
+    internal readonly struct AsyncExecutableFunc<T1, T2, TResult> : IAsyncExecutable<TResult>
+    {
+        private readonly Func<Context, CancellationToken, bool, T1, T2, Task<TResult>> _func;
+        private readonly T1 _arg1;
+        private readonly T2 _arg2;
+
+        /// <summary>
+        /// Creates a <see cref="AsyncExecutableFunc{T1, TResult}"/> instance combining a func and an input parameter of type <typeparamref name="T1"/> to be used when executing it. 
+        /// This func may be executed through a policy at a later point in time.
+        /// <remarks>Combining the action and its input parameter into a short-lived struct, as shown here, allows users to pass data to actions executed through Polly policies without using closures, which would result in extra heap allocations.</remarks>
+        /// </summary>
+        /// <param name="func">The function.</param>
+        /// <param name="arg1">The parameter to pass, when executing the function.</param>
+        /// <param name="arg2">The parameter to pass, when executing the action.</param>
+        public AsyncExecutableFunc(Func<Context, CancellationToken, bool, T1, T2, Task<TResult>> func, T1 arg1, T2 arg2)
+        {
+            _func = func;
+            _arg1 = arg1;
+            _arg2 = arg2;
+        }
+
+        /// <inheritdoc/>
+        public Task<TResult> ExecuteAsync(Context context, CancellationToken cancellationToken, bool continueOnCapturedContext) => _func(context, cancellationToken, continueOnCapturedContext, _arg1, _arg2);
+    }
+}
---
+++ src/Polly/IAsyncExecutable.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Polly
+{
+    /// <summary>
+    /// Defines an operation that can be executed asynchronously to return a promise of a result of type <typeparamref name="TResult"/>
+    /// </summary>
+    /// <typeparam name="TResult">The return type of the operation.</typeparam>
+    public interface IAsyncExecutable<TResult>
+    {
+        /// <summary>
+        /// Asynchronously executes the operation represented by the instance.
+        /// </summary>
+        /// <param name="context">The Polly execution <see cref="Context"/> to execute the operation with.</param>
+        /// <param name="cancellationToken">A <see cref="CancellationToken"/> governing cancellation of the executed operation.</param>
+        /// <param name="continueOnCapturedContext">Whether continuing after an awaited operation should continue of a captured <see cref="SynchronizationContext"/></param>
+        /// <returns>A promise of a result of type <typeparamref name="TResult"/></returns>
+        Task<TResult> ExecuteAsync(Context context, CancellationToken cancellationToken, bool continueOnCapturedContext);
+    }
+}
