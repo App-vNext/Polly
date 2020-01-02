3070045ff658bdf6044b71bd3eb96367cd9e11a9
reisenberger <software@reisenberger.net>
2020-01-02 21:44:59 +0000

    Test sync execution overloads with custom inputs

---
+++ src/Polly.Specs/Executables/ExecutablesSyncSpecs.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Threading;
+using FluentAssertions;
+using Polly.NoOp;
+using Xunit;
+
+namespace Polly.Specs.Executables
+{
+    public class ExecutablesSyncSpecs
+    {
+        [Fact]
+        public void Should_execute_user_delegate_with_input_parameter()
+        {
+            NoOpPolicy policy = Policy.NoOp() as NoOpPolicy;
+
+            Int16 input1 = 1;
+            Int64 captured = 0;
+
+            policy.Execute<Int16>((context, token, input) =>
+                {
+                    captured = input;
+                }, new Context(),
+                CancellationToken.None,
+                input1);
+
+            captured.Should().Be(input1);
+        }
+
+        [Fact]
+        public void Should_execute_user_delegate_with_two_input_parameters()
+        {
+            NoOpPolicy policy = Policy.NoOp() as NoOpPolicy;
+
+            Int16 input1 = 1;
+            Int32 input2 = 2;
+            Int64 captured = 0;
+
+            policy.Execute<Int16, Int32>((context, token, t1, t2) =>
+                {
+                    captured = t1 + t2;
+                }, new Context(),
+                CancellationToken.None,
+                input1,
+                input2);
+
+            captured.Should().Be(input1 + input2);
+        }
+
+        [Fact]
+        public void Should_execute_user_delegate_with_input_parameter_and_return_type()
+        {
+            NoOpPolicy policy = Policy.NoOp() as NoOpPolicy;
+
+            Int16 input1 = 1;
+            double captured = policy.Execute<Int16, Int64>((context, token, input) => input, new Context(),
+                CancellationToken.None,
+                input1);
+
+            captured.Should().Be(input1);
+        }
+
+        [Fact]
+        public void Should_execute_user_delegate_with_two_input_parameters_and_return_type()
+        {
+            NoOpPolicy policy = Policy.NoOp() as NoOpPolicy;
+
+            Int16 input1 = 1;
+            Int32 input2 = 2;
+            double captured = policy.Execute<Int16, Int32, Int64>((context, token, t1, t2) => t1 + t2, new Context(),
+                CancellationToken.None,
+                input1,
+                input2);
+
+            captured.Should().Be(input1 + input2);
+        }
+
+        [Fact]
+        public void Should_executeandcapture_user_delegate_with_input_parameter()
+        {
+            NoOpPolicy policy = Policy.NoOp() as NoOpPolicy;
+
+            Int16 input1 = 1;
+            double captured = 0;
+
+            var policyResult = policy.ExecuteAndCapture<Int16>((context, token, input) =>
+                {
+                    captured = input;
+                }, new Context(),
+                CancellationToken.None,
+                input1);
+
+            policyResult.Outcome.Should().Be(OutcomeType.Successful);
+            captured.Should().Be(input1);
+        }
+
+        [Fact]
+        public void Should_executeandcapture_user_delegate_with_two_input_parameters()
+        {
+            NoOpPolicy policy = Policy.NoOp() as NoOpPolicy;
+
+            Int16 input1 = 1;
+            Int32 input2 = 2;
+            double captured = 0;
+
+            var policyResult = policy.ExecuteAndCapture<Int16, Int32>((context, token, t1, t2) =>
+                {
+                    captured = t1 + t2;
+                }, new Context(),
+                CancellationToken.None,
+                input1,
+                input2);
+
+            policyResult.Outcome.Should().Be(OutcomeType.Successful);
+            captured.Should().Be(input1 + input2);
+        }
+
+        [Fact]
+        public void Should_executeandcapture_user_delegate_with_input_parameter_and_return_type()
+        {
+            NoOpPolicy policy = Policy.NoOp() as NoOpPolicy;
+
+            Int16 input1 = 1;
+            var policyResult = policy.ExecuteAndCapture<Int16, Int64>((context, token, input) => input, new Context(),
+                CancellationToken.None,
+                input1);
+
+            policyResult.Outcome.Should().Be(OutcomeType.Successful);
+            policyResult.Result.Should().Be(input1);
+        }
+
+        [Fact]
+        public void Should_executeandcapture_user_delegate_with_two_input_parameters_and_return_type()
+        {
+            NoOpPolicy policy = Policy.NoOp() as NoOpPolicy;
+
+            Int16 input1 = 1;
+            Int32 input2 = 2;
+            var policyResult = policy.ExecuteAndCapture<Int16, Int32, Int64>((context, token, t1, t2) => t1 + t2, new Context(),
+                CancellationToken.None,
+                input1,
+                input2);
+
+            policyResult.Outcome.Should().Be(OutcomeType.Successful);
+            policyResult.Result.Should().Be(input1 + input2);
+        }
+    }
+}
---
+++ src/Polly.Specs/Executables/ExecutablesTResultSyncSpecs.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Threading;
+using FluentAssertions;
+using Polly.NoOp;
+using Xunit;
+
+namespace Polly.Specs.Executables
+{
+    public class ExecutablesTResultSyncSpecs
+    {
+        [Fact]
+        public void Should_execute_user_delegate_with_input_parameter_and_return_type()
+        {
+            NoOpPolicy<Int64> policy = Policy.NoOp<Int64>() as NoOpPolicy<Int64>;
+
+            Int16 input1 = 1;
+            Int64 captured = policy.Execute<Int16>((context, token, input) => input, new Context(),
+                CancellationToken.None,
+                input1);
+
+            captured.Should().Be(input1);
+        }
+
+        [Fact]
+        public void Should_execute_user_delegate_with_two_input_parameters_and_return_type()
+        {
+            NoOpPolicy<Int64> policy = Policy.NoOp<Int64>() as NoOpPolicy<Int64>;
+
+            Int16 input1 = 1;
+            Int32 input2 = 2;
+            Int64 captured = policy.Execute<Int16, Int32>((context, token, t1, t2) => t1 + t2, new Context(),
+                CancellationToken.None,
+                input1,
+                input2);
+
+            captured.Should().Be(input1 + input2);
+        }
+        
+        [Fact]
+        public void Should_executeandcapture_user_delegate_with_input_parameter_and_return_type()
+        {
+            NoOpPolicy<Int64> policy = Policy.NoOp<Int64>() as NoOpPolicy<Int64>;
+
+            Int16 input1 = 1;
+            var policyResult = policy.ExecuteAndCapture<Int16>((context, token, input) => input, new Context(),
+                CancellationToken.None,
+                input1);
+
+            policyResult.Outcome.Should().Be(OutcomeType.Successful);
+            policyResult.Result.Should().Be(input1);
+        }
+
+        [Fact]
+        public void Should_executeandcapture_user_delegate_with_two_input_parameters_and_return_type()
+        {
+            NoOpPolicy<Int64> policy = Policy.NoOp<Int64>() as NoOpPolicy<Int64>;
+
+            Int16 input1 = 1;
+            Int32 input2 = 2;
+            var policyResult = policy.ExecuteAndCapture<Int16, Int32>((context, token, t1, t2) => t1 + t2, new Context(),
+                CancellationToken.None,
+                input1,
+                input2);
+
+            policyResult.Outcome.Should().Be(OutcomeType.Successful);
+            policyResult.Result.Should().Be(input1 + input2);
+        }
+    }
+}
