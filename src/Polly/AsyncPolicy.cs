1eadf17ec7f855738955d03af59f2b326cfa4b57
reisenberger <software@reisenberger.net>
2020-01-11 23:57:57 +0000

    Remove dead code

--- src/Polly/AsyncPolicy.cs
+++ src/Polly/AsyncPolicy.cs
@@ -3,26 +3,8 @@
     /// <summary>
     /// Transient exception handling policies that can be applied to asynchronous delegates
     /// </summary>
-    public abstract /*partial*/ class AsyncPolicy
+    public abstract class AsyncPolicy
     {
-    /*
-        /// <summary>
-        /// Constructs a new instance of a derived <see cref="AsyncPolicy"/> type with the passed <paramref name="exceptionPredicates"/>. 
-        /// </summary>
-        /// <param name="exceptionPredicates">Predicates indicating which exceptions the policy should handle. </param>
-        internal AsyncPolicy(ExceptionPredicates exceptionPredicates)
-            : base(exceptionPredicates)
-        {
-        }
-
-        /// <summary>
-        /// Constructs a new instance of a derived <see cref="AsyncPolicy"/> type with the passed <paramref name="policyBuilder"/>. 
-        /// </summary>
-        /// <param name="policyBuilder">A <see cref="PolicyBuilder"/> specifying which exceptions the policy should handle. </param>
-        protected AsyncPolicy(PolicyBuilder policyBuilder = null)
-            : base(policyBuilder)
-        {
-        }
-*/
+        // Retained only because referenced in intellisense.  Will be deleted when AsyncPolicyV8 renamed to AsyncPolicy.
     }
 }
