83a8a2b29f91fa48a8cabaab3a8aafa2e9d8c869
martincostello <martin@martincostello.com>
2021-04-17 14:30:42 +0100

    Add benchmarks project
    Add basic benchmarks project for comparing the latest* NuGet version of Polly with the local project.

---
+++ src/Polly.Benchmarks/Program.cs
@@ -0,0 +1,4 @@
+﻿using System.Reflection;
+using BenchmarkDotNet.Running;
+
+BenchmarkSwitcher.FromAssembly(Assembly.GetCallingAssembly()).RunAll();
---
+++ src/Polly.Benchmarks/RetryBenchmarks.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+
+namespace Polly.Benchmarks
+{
+    [Config(typeof(PollyConfig))]
+    public class RetryBenchmarks
+    {
+        [Benchmark]
+        public void Retry_Synchronous()
+        {
+            var policy = Policy.Handle<InvalidOperationException>().Retry();
+
+            int count = 0;
+
+            policy.Execute(() =>
+            {
+                if (count++ % 2 == 0)
+                {
+                    throw new InvalidOperationException();
+                }
+            });
+        }
+
+        [Benchmark]
+        public async Task Retry_Aynchronous()
+        {
+            var policy = Policy.Handle<InvalidOperationException>().RetryAsync();
+
+            int count = 0;
+
+            await policy.ExecuteAsync(() =>
+            {
+                if (count++ % 2 == 0)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                return Task.CompletedTask;
+            });
+        }
+    }
+}
