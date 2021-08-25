ca8f51f987cdce9464f1eee025142d5c0b4a1a92
Martin Costello <martin@martincostello.com>
2021-08-25 10:18:16 +0100

    Add Benchmarks project (#864)
    * Add benchmarks project
    
    Add basic benchmarks project for comparing the latest* NuGet version of Polly with the local project.
    
    * Extend retry benchmarks
    
    Extend the retry benchmarks for more usages.
    
    * Rename class
    
    Remove the Benchmarks suffix.
    
    * More benchmarks
    
    Add more benchmarks for different policies.
    Reuse the policies in the Retry benchmarks.
    
    * Remove broken bechmark
    
    Remove benchmark that just hangs.
    
    * Add cache benchmarks
    
    Add benchmarks for the cache policy using the implementation from Polly.Caching.MemoryCache.
    
    * Remove EventPipeProfiler
    
    Remove EventPipeProfiler as it doesn't seem to report anything that useful.
    
    * Remove another hanging benchmark
    
    Remove another benchmark that just hangs indefinitely.
    
    * Add bulkhead to wrap benchmark
    
    Also add a bulkhead policy to the PolicyWrap benchmarks.
    
    * Update BenchmarkDotNet
    
    Update to the latest release of BenchmarkDotNet.
    
    * Use Task.CompletedTask
    
    Use Task.CompletedTask in .NET versions that support it.
    
    * Use TryGetValue()
    
    Use TryGetValue() with dictionary, instead of ContainsKey() and the indexer.
    
    * Use LangVersion latest
    
    Set LangVersion to latest now that BenchmarkDotNet supports it.

---
+++ src/Polly.Benchmarks/NoOp.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+
+namespace Polly.Benchmarks
+{
+    [Config(typeof(PollyConfig))]
+    public class NoOp
+    {
+        private static readonly Policy SyncPolicy = Policy.NoOp();
+        private static readonly AsyncPolicy AsyncPolicy = Policy.NoOpAsync();
+
+        [Benchmark]
+        public void NoOp_Synchronous()
+        {
+            SyncPolicy.Execute(() => Workloads.Action());
+        }
+
+        [Benchmark]
+        public async Task NoOp_Asynchronous()
+        {
+            await AsyncPolicy.ExecuteAsync((token) => Workloads.ActionAsync(token), CancellationToken.None);
+        }
+
+        [Benchmark]
+        public int NoOp_Synchronous_With_Result()
+        {
+            return SyncPolicy.Execute(() => Workloads.Func<int>());
+        }
+
+        [Benchmark]
+        public async Task<int> NoOp_Asynchronous_With_Result()
+        {
+            return await AsyncPolicy.ExecuteAsync((token) => Workloads.FuncAsync<int>(token), CancellationToken.None);
+        }
+    }
+}
