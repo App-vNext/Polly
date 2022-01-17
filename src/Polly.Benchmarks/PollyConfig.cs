b1a24d9d5a5824077a0710bfac8523d01ae62a94
martincostello <martin@martincostello.com>
2022-01-17 10:38:35 +0000

    Add RateLimit benchmarks
    Add benchmarks for the rate-limit policy.

--- src/Polly.Benchmarks/PollyConfig.cs
+++ src/Polly.Benchmarks/PollyConfig.cs
@@ -28,7 +28,7 @@ namespace Polly.Benchmarks
 
             if (useNuGet)
             {
-                result = result.WithNuGet("Polly", "7.2.1");
+                result = result.WithNuGet("Polly", "7.3.0");
             }
 
             return result;
