7c51f6f7158cd632f92df5f60cbcd38b37836c1c
neptune <eugeneogongo@live.com>
2022-02-11 23:16:09 +0300

    Disable bug exceptoin

--- build.ps1
+++ build.ps1
@@ -129,5 +129,5 @@ if(-Not $SkipToolPackageRestore.IsPresent)
 
 # Start Cake
 Write-Host "Running build script..."
-Invoke-Expression "$CAKE_EXE `"$Script`" --target=`"$Target`" --IGNORE_NORMALISATION_GIT_HEAD_MOVE="1" --configuration=`"$Configuration`" --verbosity=`"$Verbosity`" $UseMono $UseDryRun $UseExperimental"
+Invoke-Expression "$CAKE_EXE `"$Script`" --target=`"$Target`" --IGNORE_NORMALISATION_GIT_HEAD_MOVE=`"1`" --configuration=`"$Configuration`" --verbosity=`"$Verbosity`" $UseMono $UseDryRun $UseExperimental"
 exit $LASTEXITCODE
