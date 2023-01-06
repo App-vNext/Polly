a6f55a8bd7fa3224104a7dc4d3a8c9dff57e35a0
martincostello <martin@martincostello.com>
2023-01-06 22:39:14 +0000

    Build on Linux and macOS
    - Update the build.ps1 script to support running on non-Windows platforms.
    - Build on Linux and macOS in GitHub Actions.
    - Remove redundant parameters from `build.ps1`.

--- build.ps1
+++ build.ps1
@@ -2,10 +2,10 @@
 <#
 
 .SYNOPSIS
-This is a Powershell script to bootstrap a Cake build.
+This is a PowerShell script to bootstrap a Cake build.
 
 .DESCRIPTION
-This Powershell script will download NuGet if missing, restore NuGet tools (including Cake)
+This PowerShell script will restore NuGet tools (including Cake)
 and execute your Cake build script with the parameters you provide.
 
 .PARAMETER Script
@@ -16,13 +16,9 @@ The build script target to run.
 The build configuration to use.
 .PARAMETER Verbosity
 Specifies the amount of information to be displayed.
-.PARAMETER Experimental
-Tells Cake to use the latest Roslyn release.
 .PARAMETER WhatIf
 Performs a dry run of the build script.
 No tasks will be executed.
-.PARAMETER Mono
-Tells Cake to use the Mono scripting engine.
 
 .LINK
 http://cakebuild.net
@@ -34,10 +30,8 @@ Param(
     [string]$Configuration = "Release",
     [ValidateSet("Quiet", "Minimal", "Normal", "Verbose", "Diagnostic")]
     [string]$Verbosity = "Verbose",
-    [switch]$Experimental,
     [Alias("DryRun","Noop")]
     [switch]$WhatIf,
-    [switch]$Mono,
     [switch]$SkipToolPackageRestore,
     [switch]$Verbose
 )
@@ -54,23 +48,6 @@ if ($Verbose.IsPresent)
 }
 
 $TOOLS_DIR = Join-Path $PSScriptRoot "tools"
-$NUGET_EXE = Join-Path $TOOLS_DIR "nuget.exe"
-$PACKAGES_CONFIG = Join-Path $TOOLS_DIR "packages.config"
-$DOTNET = "dotnet.exe"
-
-# Should we use mono?
-$UseMono = "";
-if ($Mono.IsPresent) {
-    Write-Verbose -Message "Using the Mono based scripting engine."
-    $UseMono = "-mono"
-}
-
-# Should we use the new Roslyn?
-$UseExperimental = "";
-if ($Experimental.IsPresent -and !($Mono.IsPresent)) {
-    Write-Verbose -Message "Using experimental version of Roslyn."
-    $UseExperimental = "-experimental"
-}
 
 # Is this a dry run?
 $UseDryRun = "";
@@ -83,20 +60,6 @@ if ((Test-Path $PSScriptRoot) -and !(Test-Path $TOOLS_DIR)) {
     New-Item -Path $TOOLS_DIR -Type directory | out-null
 }
 
-# Try download NuGet.exe if not exists
-if (!(Test-Path $NUGET_EXE)) {
-    Write-Verbose -Message "Downloading NuGet.exe..."
-    Invoke-WebRequest -Uri https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -OutFile $NUGET_EXE
-}
-
-# Make sure NuGet exists where we expect it.
-if (!(Test-Path $NUGET_EXE)) {
-    Throw "Could not find NuGet.exe"
-}
-
-# Save nuget.exe path to environment to be available to child processed
-$ENV:NUGET_EXE = $NUGET_EXE
-
 # Restore tools from NuGet?
 if (-Not $SkipToolPackageRestore.IsPresent)
 {
@@ -106,18 +69,9 @@ if (-Not $SkipToolPackageRestore.IsPresent)
 
     Write-Verbose -Message "Restoring tools from NuGet..."
 
-    # Restore packages
-    if (Test-Path $PACKAGES_CONFIG)
-    {
-        $NuGetOutput = Invoke-Expression "&`"$NUGET_EXE`" install -ExcludeVersion"
-        Write-Verbose ($NuGetOutput | Out-String)
-    }
-    # Install just Cake if missing config
-    else
-    {
-        $NuGetOutput = Invoke-Expression "&`"$DOTNET`" tool install Cake.Tool --version 3.0.0"
-        Write-Verbose ($NuGetOutput | Out-String)
-    }
+    $NuGetOutput = Invoke-Expression "& dotnet tool install Cake.Tool --version 3.0.0"
+    Write-Verbose ($NuGetOutput | Out-String)
+
     Pop-Location
     if ($LASTEXITCODE -ne 0)
     {
@@ -127,5 +81,5 @@ if (-Not $SkipToolPackageRestore.IsPresent)
 
 # Start Cake
 Write-Host "Running build script..."
-Invoke-Expression "dotnet dotnet-cake `"$Script`" --target=`"$Target`" --configuration=`"$Configuration`" --verbosity=`"$Verbosity`" $UseMono $UseDryRun $UseExperimental"
+Invoke-Expression "dotnet dotnet-cake `"$Script`" --target=`"$Target`" --configuration=`"$Configuration`" --verbosity=`"$Verbosity`" $UseDryRun"
 exit $LASTEXITCODE
