15dd4d3333867484c5b98227b3564f359fd4a5a7
Simon Cropp <simon.cropp@gmail.com>
2023-01-15 09:36:19 +1100

    enable nullable for SingleHealthMetrics

--- src/Polly/CircuitBreaker/SingleHealthMetrics.cs
+++ src/Polly/CircuitBreaker/SingleHealthMetrics.cs
@@ -1,44 +1,38 @@
-﻿namespace Polly.CircuitBreaker;
+﻿#nullable enable
+
+namespace Polly.CircuitBreaker;
 
 internal class SingleHealthMetrics : IHealthMetrics
 {
     private readonly long _samplingDuration;
 
-    private HealthCount _current;
+    private HealthCount? _current;
 
     public SingleHealthMetrics(TimeSpan samplingDuration) =>
         _samplingDuration = samplingDuration.Ticks;
 
-    public void IncrementSuccess_NeedsLock()
-    {
-        ActualiseCurrentMetric_NeedsLock();
+    public void IncrementSuccess_NeedsLock() =>
+        ActualiseCurrentMetric_NeedsLock().Successes++;
 
-        _current.Successes++;
-    }
-
-    public void IncrementFailure_NeedsLock()
-    {
-        ActualiseCurrentMetric_NeedsLock();
-
-        _current.Failures++;
-    }
+    public void IncrementFailure_NeedsLock() =>
+        ActualiseCurrentMetric_NeedsLock().Failures++;
 
     public void Reset_NeedsLock() =>
         _current = null;
 
-    public HealthCount GetHealthCount_NeedsLock()
-    {
+    public HealthCount GetHealthCount_NeedsLock() =>
         ActualiseCurrentMetric_NeedsLock();
 
-        return _current;
-    }
-
-    private void ActualiseCurrentMetric_NeedsLock()
+    private HealthCount ActualiseCurrentMetric_NeedsLock()
     {
         long now = SystemClock.UtcNow().Ticks;
         if (_current == null || now - _current.StartedAt >= _samplingDuration)
         {
-            _current = new HealthCount { StartedAt = now };
+            _current = new()
+            {
+                StartedAt = now
+            };
         }
+        return _current;
     }
 }
