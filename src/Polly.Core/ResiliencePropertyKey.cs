2162debb5bd2cef9df6878ad7178fd7b6c0c7cdc
martintmk <martintomka@microsoft.com>
2023-03-20 09:00:28 +0100

    Add equality members

--- src/Polly.Core/ResiliencePropertyKey.cs
+++ src/Polly.Core/ResiliencePropertyKey.cs
@@ -1,12 +1,10 @@
 namespace Polly;
 
-#pragma warning disable CA1815 // Override equals and operator equals on value types, this type should never be used as key to dictionary
-
 /// <summary>
 /// Represents a key used by <see cref="ResilienceProperties"/>.
 /// </summary>
 /// <typeparam name="TValue">The type of the value of the property.</typeparam>
-public readonly struct ResiliencePropertyKey<TValue>
+public readonly struct ResiliencePropertyKey<TValue> : IEquatable<ResiliencePropertyKey<TValue>>
 {
     /// <summary>
     /// Initializes a new instance of the <see cref="ResiliencePropertyKey{TValue}"/> struct.
@@ -26,5 +24,30 @@ public readonly struct ResiliencePropertyKey<TValue>
 
     /// <inheritdoc/>
     public override string ToString() => Key;
+
+    /// /// <inheritdoc/>
+    public override bool Equals(object obj) => obj is ResiliencePropertyKey<TValue> other && Equals(other);
+
+    /// <inheritdoc/>
+    public bool Equals(ResiliencePropertyKey<TValue> other) => StringComparer.Ordinal.Equals(Key, other.Key);
+
+    /// <inheritdoc/>
+    public override int GetHashCode() => (StringComparer.Ordinal.GetHashCode(Key), typeof(TValue)).GetHashCode();
+
+    /// <summary>
+    /// The operator to compare two instances of <see cref="ResiliencePropertyKey{TValue}"/> for equality.
+    /// </summary>
+    /// <param name="left">The left instance.</param>
+    /// <param name="right">The right instance.</param>
+    /// <returns>True if the instances are equal, false otherwise.</returns>
+    public static bool operator ==(ResiliencePropertyKey<TValue> left, ResiliencePropertyKey<TValue> right) => left.Equals(right);
+
+    /// <summary>
+    /// The operator to compare two instances of <see cref="ResiliencePropertyKey{TValue}"/> for inequality.
+    /// </summary>
+    /// <param name="left">The left instance.</param>
+    /// <param name="right">The right instance.</param>
+    /// <returns>True if the instances are not equal, false otherwise.</returns>
+    public static bool operator !=(ResiliencePropertyKey<TValue> left, ResiliencePropertyKey<TValue> right) => !(left == right);
 }
 
