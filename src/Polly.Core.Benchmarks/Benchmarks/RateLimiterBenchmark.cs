cfd3d56be8b7a098755a1e8603d9b0bac76a9810
martintmk <103487740+martintmk@users.noreply.github.com>
2023-04-11 19:15:16 +0200

    Add more benchmarks (#1106)

---
+++ src/Polly.Core.Benchmarks/Benchmarks/RateLimiterBenchmark.cs
@@ -0,0 +1,24 @@
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using Polly.Core.Benchmarks;
+
+namespace Polly.Benchmarks;
+
+public class RateLimiterBenchmark
+{
+    private object? _strategyV7;
+    private object? _strategyV8;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        _strategyV7 = Helper.CreateRateLimiter(PollyVersion.V7);
+        _strategyV8 = Helper.CreateRateLimiter(PollyVersion.V8);
+    }
+
+    [Benchmark(Baseline = true)]
+    public ValueTask ExecuteRateLimiter_V7() => _strategyV7!.ExecuteAsync(PollyVersion.V7);
+
+    [Benchmark]
+    public ValueTask ExecuteRateLimiter_V8() => _strategyV8!.ExecuteAsync(PollyVersion.V8);
+}
