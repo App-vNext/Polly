de77014e5347d4c985674afe9b6918497a1bf37c
martintmk <martintomka@microsoft.com>
2023-04-12 09:51:18 +0200

    Fixes

--- src/Polly.Core.Tests/ExecutionRejectedExceptionTests.cs
+++ src/Polly.Core.Tests/ExecutionRejectedExceptionTests.cs
@@ -5,7 +5,7 @@ public class ExecutionRejectedExceptionTests
     [Fact]
     public void Ctor_Ok()
     {
-        new CustomException().Message.Should().Be("Exception of type 'Polly.Core.Tests.Timeout.ExecutionRejectedExceptionTests+CustomException' was thrown.");
+        new CustomException().Message.Should().Be("Exception of type 'Polly.Core.Tests.ExecutionRejectedExceptionTests+CustomException' was thrown.");
         new CustomException("Dummy").Message.Should().Be("Dummy");
     }
 
