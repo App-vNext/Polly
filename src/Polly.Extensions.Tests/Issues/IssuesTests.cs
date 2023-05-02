95dd68dce22349dde7ca9f2ad09942a2249be598
martintmk <martintomka@microsoft.com>
2023-05-02 10:29:38 +0200

    build fix

--- src/Polly.Extensions.Tests/Issues/IssuesTests.cs
+++ src/Polly.Extensions.Tests/Issues/IssuesTests.cs
@@ -1,8 +1,3 @@
-using Microsoft.Extensions.DependencyInjection;
-using Polly.CircuitBreaker;
-using Polly.Extensions.DependencyInjection;
-using Polly.Registry;
-
 namespace Polly.Core.Tests.Issues;
 
 public partial class IssuesTests
