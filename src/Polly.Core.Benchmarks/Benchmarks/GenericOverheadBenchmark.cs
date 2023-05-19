92083bb71f7ee97afc63687dc370389c0a7f537f
martintmk <103487740+martintmk@users.noreply.github.com>
2023-05-19 16:26:32 +0200

    Add a generics benchmark (#1202)

---
+++ src/Polly.Core.Benchmarks/Benchmarks/GenericOverheadBenchmark.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+
+namespace Polly.Benchmarks;
+
+public class GenericOverheadBenchmark
+{
+    private readonly GenericStrategy<string> _generic;
+    private readonly NonGenericStrategy _nonGeneric;
+
+    public GenericOverheadBenchmark()
+    {
+        _generic = new GenericStrategy<string>();
+        _nonGeneric = new NonGenericStrategy();
+    }
+
+    [Benchmark(Baseline = true)]
+    public async ValueTask ExecuteAsync_Generic() => await _generic.ExecuteAsync(static () => new ValueTask<string>("dummy")).ConfigureAwait(false);
+
+    [Benchmark]
+    public async ValueTask ExecuteAsync_NonGeneric() => await _nonGeneric.ExecuteAsync(static () => new ValueTask<string>("dummy")).ConfigureAwait(false);
+
+    public class GenericStrategy<T>
+    {
+        [MethodImpl(MethodImplOptions.NoOptimization)]
+        public virtual ValueTask<T> ExecuteAsync(Func<ValueTask<T>> callback)
+        {
+            return callback();
+        }
+    }
+
+    public class NonGenericStrategy
+    {
+        [MethodImpl(MethodImplOptions.NoOptimization)]
+        public virtual ValueTask<T> ExecuteAsync<T>(Func<ValueTask<T>> callback)
+        {
+            return callback();
+        }
+    }
+}
