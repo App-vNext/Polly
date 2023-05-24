68b86a907868ce09023661f1db12566f913578a0
Martin Tomka <martintomka@microsoft.com>
2023-05-24 11:05:43 +0200

    Code coverage fixes.

--- src/Polly.Core/Strategy/NoOutcomeGenerator.cs
+++ src/Polly.Core/Strategy/NoOutcomeGenerator.cs
@@ -11,11 +11,6 @@ public sealed class NoOutcomeGenerator<TArgs, TValue>
     private Func<TArgs, ValueTask<TValue>>? _generator;
 
     /// <summary>
-    /// Gets a value indicating whether the generator is empty.
-    /// </summary>
-    internal bool IsEmpty => _generator is null;
-
-    /// <summary>
     /// Adds a result generator.
     /// </summary>
     /// <param name="generator">The generator to determine if the result should be retried.</param>
