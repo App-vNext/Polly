2e8dfebd3e02c7d9a4cd6072f91097ae6de9041c
Martin Tomka <martintomka@microsoft.com>
2023-06-13 09:19:09 +0200

    Drop some convienience extensions

--- src/Polly.Core/Timeout/TimeoutResilienceStrategyBuilderExtensions.cs
+++ src/Polly.Core/Timeout/TimeoutResilienceStrategyBuilderExtensions.cs
@@ -33,35 +33,6 @@ public static class TimeoutResilienceStrategyBuilderExtensions
     /// </summary>
     /// <typeparam name="TBuilder">The builder type.</typeparam>
     /// <param name="builder">The builder instance.</param>
-    /// <param name="timeout">The timeout value. This value should be greater than <see cref="TimeSpan.Zero"/> or <see cref="System.Threading.Timeout.InfiniteTimeSpan"/>.</param>
-    /// <param name="onTimeout">The callback that is executed when timeout happens.</param>
-    /// <returns>The same builder instance.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="builder"/> or <paramref name="onTimeout"/> is <see langword="null"/>.</exception>
-    /// <exception cref="ValidationException">Thrown when the options produced from the arguments are invalid.</exception>
-    public static TBuilder AddTimeout<TBuilder>(this TBuilder builder, TimeSpan timeout, Action<OnTimeoutArguments> onTimeout)
-        where TBuilder : ResilienceStrategyBuilderBase
-    {
-        Guard.NotNull(builder);
-        Guard.NotNull(onTimeout);
-
-        builder.AddTimeout(new TimeoutStrategyOptions
-        {
-            Timeout = timeout,
-            OnTimeout = (args) =>
-            {
-                onTimeout(args);
-                return default;
-            }
-        });
-
-        return builder;
-    }
-
-    /// <summary>
-    /// Adds a timeout resilience strategy to the builder.
-    /// </summary>
-    /// <typeparam name="TBuilder">The builder type.</typeparam>
-    /// <param name="builder">The builder instance.</param>
     /// <param name="options">The timeout options.</param>
     /// <returns>The same builder instance.</returns>
     /// <exception cref="ArgumentNullException">Thrown when <paramref name="builder"/> or <paramref name="options"/> is <see langword="null"/>.</exception>
