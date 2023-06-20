29509ddbdd398b60b6764e5a954e3934ebd7822d
Martin Tomka <martintomka@microsoft.com>
2023-06-20 08:27:37 +0200

    code coverage

--- test/Polly.Core.Tests/Telemetry/ExecutionAttemptArgumentsTests.cs
+++ test/Polly.Core.Tests/Telemetry/ExecutionAttemptArgumentsTests.cs
@@ -6,6 +6,16 @@ namespace Polly.Extensions.Tests.Telemetry;
 public class ExecutionAttemptArgumentsTests
 {
     [Fact]
+    public void Ctor_Ok()
+    {
+        var args = new ExecutionAttemptArguments(99, TimeSpan.MaxValue, true);
+        Assert.NotNull(args);
+        args.Attempt.Should().Be(99);
+        args.ExecutionTime.Should().Be(TimeSpan.MaxValue);
+        args.Handled.Should().BeTrue();
+    }
+
+    [Fact]
     public void Get_Ok()
     {
         var args = ExecutionAttemptArguments.Get(99, TimeSpan.MaxValue, true);
