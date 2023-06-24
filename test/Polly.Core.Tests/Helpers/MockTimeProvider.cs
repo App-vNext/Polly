761d96966a2725ed72ee35b04c0f0af211a640d5
martincostello <martin@martincostello.com>
2023-06-24 15:02:51 +0100

    Remove unused MockTimeProvider methods
    Remove methods on `MockTimeProvider` that are unsed after the adoption of `FakeTimeProvider`.

--- test/Polly.Core.Tests/Helpers/MockTimeProvider.cs
+++ test/Polly.Core.Tests/Helpers/MockTimeProvider.cs
@@ -4,46 +4,14 @@ namespace Polly.Core.Tests.Helpers;
 
 internal class MockTimeProvider : Mock<TimeProvider>
 {
-    private DateTimeOffset? _time;
-
     public MockTimeProvider()
         : base(MockBehavior.Strict)
     {
     }
 
-    public MockTimeProvider SetupUtcNow(DateTimeOffset? time = null)
-    {
-        _time = time ?? DateTimeOffset.UtcNow;
-        Setup(x => x.GetUtcNow()).Returns(() => _time.Value);
-        return this;
-    }
-
-    public MockTimeProvider AdvanceTime(TimeSpan time)
-    {
-        if (_time == null)
-        {
-            SetupUtcNow(DateTimeOffset.UtcNow);
-        }
-
-        _time = _time!.Value.Add(time);
-        return this;
-    }
-
-    public MockTimeProvider SetupTimestampFrequency()
-    {
-        Setup(x => x.TimestampFrequency).Returns(Stopwatch.Frequency);
-        return this;
-    }
-
     public MockTimeProvider SetupAnyDelay(CancellationToken cancellationToken = default)
     {
         Setup(x => x.Delay(It.IsAny<TimeSpan>(), cancellationToken)).Returns(Task.CompletedTask);
-        return this;
-    }
-
-    public MockTimeProvider SetupGetTimestamp()
-    {
-        Setup(x => x.GetTimestamp()).Returns(0);
         return this;
     }
 
