9a69db3289058f6115cacd2e3a7e614b5de637f0
Martin Tomka <martintomka@microsoft.com>
2023-06-29 09:04:10 +0200

    Demonstrate how to create dynamic strategies with complex keys

---
+++ test/Polly.Extensions.Tests/Issues/IssuesTests.StrategiesPerEndpoint_1365.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Net.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Polly.Registry;
+using Polly.Retry;
+using Polly.Timeout;
+
+namespace Polly.Extensions.Tests.Issues;
+
+public partial class IssuesTests
+{
+    [Fact]
+    public void StrategiesPerEndpoint_1365()
+    {
+        var services = new ServiceCollection();
+
+        // add resilience strategy, keyed by EndpointKey that only defines the builder name
+        services.AddResilienceStrategy<EndpointKey, HttpResponseMessage>(new EndpointKey("my-pipeline", string.Empty, string.Empty, new()), (builder, context) =>
+        {
+            var endpointOptions = context.StrategyKey.EndpointOptions;
+
+            // apply retries optionally per-endpoint
+            if (endpointOptions.Retries > 0)
+            {
+                builder.AddRetry(new()
+                {
+                    BackoffType = RetryBackoffType.ExponentialWithJitter,
+                    RetryCount = endpointOptions.Retries,
+                    StrategyName = $"{context.StrategyKey.EndpointName}-Retry",
+                });
+            }
+
+            // apply circuit breaker
+            builder.AddAdvancedCircuitBreaker(new()
+            {
+                BreakDuration = endpointOptions.BreakDuration,
+                StrategyName = $"{context.StrategyKey.EndpointName}-{context.StrategyKey.Resource}-CircuitBreaker"
+            });
+
+            // apply timeout
+
+            builder.AddTimeout(new TimeoutStrategyOptions
+            {
+                StrategyName = $"{context.StrategyKey.EndpointName}-Timeout",
+                Timeout = endpointOptions.Timeout.Add(TimeSpan.FromSeconds(1)),
+            });
+        });
+
+        // configure the registry to allow multi-dimensional keys
+        services.Configure<ResilienceStrategyRegistryOptions<EndpointKey>>(options =>
+        {
+            options.BuilderComparer = new EndpointKey.BuilderComparer();
+            options.StrategyComparer = new EndpointKey.StrategyComparer();
+
+            // format the key for telemetry
+            options.StrategyKeyFormatter = key => $"{key.BuilderName}-{key.EndpointName}-{key.Resource}";
+
+            // format the builder name for telemetry
+            options.BuilderNameFormatter = key => key.BuilderName;
+        });
+
+        // create the strategy provider
+        var provider = services.BuildServiceProvider().GetRequiredService<ResilienceStrategyProvider<EndpointKey>>();
+
+        // Endpoint 1
+        var endpoint1Options = new EndpointOptions
+        {
+            Retries = 3,
+            BreakDuration = TimeSpan.FromSeconds(30),
+            Timeout = TimeSpan.FromSeconds(10),
+        };
+
+        // define a key for each resource/endpoint combination
+        var resource1Key = new EndpointKey("my-pipeline", "Endpoint 1", "Resource 1", endpoint1Options);
+        var resource2Key = new EndpointKey("my-pipeline", "Endpoint 1", "Resource 2", endpoint1Options);
+
+        var strategy1 = provider.GetStrategy<HttpResponseMessage>(resource1Key);
+        var strategy2 = provider.GetStrategy<HttpResponseMessage>(resource2Key);
+
+        strategy1.Should().NotBe(strategy2);
+    }
+
+    public class EndpointOptions
+    {
+        public int Retries { get; set; }
+
+        public TimeSpan BreakDuration { get; set; }
+
+        public TimeSpan Timeout { get; set; }
+    }
+
+    public record EndpointKey(string BuilderName, string EndpointName, string Resource, EndpointOptions EndpointOptions)
+    {
+        public class BuilderComparer : IEqualityComparer<EndpointKey>
+        {
+            public bool Equals(EndpointKey? x, EndpointKey? y) => StringComparer.Ordinal.Equals(x.BuilderName, y.BuilderName);
+
+            public int GetHashCode([DisallowNull] EndpointKey obj) => StringComparer.Ordinal.GetHashCode(obj.BuilderName);
+        }
+
+        public class StrategyComparer : IEqualityComparer<EndpointKey>
+        {
+            public bool Equals(EndpointKey? x, EndpointKey? y) => (x.BuilderName, x.EndpointName, x.Resource) == (y.BuilderName, y.EndpointName, y.Resource);
+
+            public int GetHashCode([DisallowNull] EndpointKey obj) => (obj.BuilderName, obj.EndpointName, obj.Resource).GetHashCode();
+        }
+    }
+}
