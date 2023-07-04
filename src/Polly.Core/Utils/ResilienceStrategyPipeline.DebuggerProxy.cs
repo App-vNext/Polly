a5007c145c315b141ef0b3a2a6462e8cd1c77010
martintmk <103487740+martintmk@users.noreply.github.com>
2023-07-04 08:15:26 +0200

    Fix debugger proxies (#1384)

---
+++ src/Polly.Core/Utils/ResilienceStrategyPipeline.DebuggerProxy.cs
@@ -0,0 +1,14 @@
+﻿namespace Polly.Utils;
+
+internal partial class ResilienceStrategyPipeline
+{
+    internal sealed class DebuggerProxy
+    {
+        private readonly ResilienceStrategyPipeline _resilienceStrategy;
+
+        public DebuggerProxy(ResilienceStrategyPipeline resilienceStrategy) => _resilienceStrategy = resilienceStrategy;
+
+        [DebuggerBrowsable(DebuggerBrowsableState.RootHidden)]
+        public IEnumerable<ResilienceStrategy> Strategies => _resilienceStrategy.Strategies;
+    }
+}
