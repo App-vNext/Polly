60db9d3f4e4b8e2a01ea1a95d71fa0005fe262c0
Geovanny Alzate Sandoval <vany0114@hotmail.com>
2023-07-09 17:30:34 -0500

    put back MonkeyStrategy generic implemetnation so that people can extend generic chaos strategies?

---
+++ src/Polly.Core/Simmy/MonkeyStrategy.TResult.cs
@@ -0,0 +1,108 @@
+﻿namespace Polly.Simmy;
+
+/// <summary>
+/// Contains common functionality for chaos strategies which intentionally disrupt executions - which monkey around with calls.
+/// </summary>
+/// <typeparam name="TResult">The type of result this strategy supports.</typeparam>
+public partial class MonkeyStrategy<TResult>
+{
+    internal MonkeyStrategy(MonkeyStrategy strategy) => Strategy = strategy;
+
+    internal MonkeyStrategy Strategy { get; }
+
+    /// <summary>
+    /// Executes the specified callback.
+    /// </summary>
+    /// <typeparam name="TState">The type of state associated with the callback.</typeparam>
+    /// <param name="callback">The user-provided callback.</param>
+    /// <param name="context">The context associated with the callback.</param>
+    /// <param name="state">The state associated with the callback.</param>
+    /// <returns>The instance of <see cref="ValueTask"/> that represents the asynchronous execution.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="callback"/> or <paramref name="context"/> is <see langword="null"/>.</exception>
+    public ValueTask<TResult> ExecuteAsync<TState>(
+        Func<ResilienceContext, TState, ValueTask<TResult>> callback,
+        ResilienceContext context,
+        TState state)
+    {
+        Guard.NotNull(callback);
+        Guard.NotNull(context);
+
+        return Strategy.ExecuteAsync(callback, context, state);
+    }
+
+    /// <summary>
+    /// Executes the specified callback.
+    /// </summary>
+    /// <param name="callback">The user-provided callback.</param>
+    /// <param name="context">The context associated with the callback.</param>
+    /// <returns>The instance of <see cref="ValueTask"/> that represents the asynchronous execution.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="callback"/> or <paramref name="context"/> is <see langword="null"/>.</exception>
+    public ValueTask<TResult> ExecuteAsync(
+        Func<ResilienceContext, ValueTask<TResult>> callback,
+        ResilienceContext context)
+    {
+        Guard.NotNull(callback);
+        Guard.NotNull(context);
+
+        return Strategy.ExecuteAsync(callback, context);
+    }
+
+    /// <summary>
+    /// Executes the specified callback.
+    /// </summary>
+    /// <typeparam name="TState">The type of state associated with the callback.</typeparam>
+    /// <param name="callback">The user-provided callback.</param>
+    /// <param name="state">The state associated with the callback.</param>
+    /// <param name="cancellationToken">The <see cref="CancellationToken"/> associated with the callback.</param>
+    /// <returns>The instance of <see cref="ValueTask"/> that represents the asynchronous execution.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="callback"/> is <see langword="null"/>.</exception>
+    public ValueTask<TResult> ExecuteAsync<TState>(
+        Func<TState, CancellationToken, ValueTask<TResult>> callback,
+        TState state,
+        CancellationToken cancellationToken = default)
+    {
+        Guard.NotNull(callback);
+
+        return Strategy.ExecuteAsync(callback, state, cancellationToken);
+    }
+
+    /// <summary>
+    /// Executes the specified callback.
+    /// </summary>
+    /// <param name="callback">The user-provided callback.</param>
+    /// <param name="cancellationToken">The <see cref="CancellationToken"/> associated with the callback.</param>
+    /// <returns>The instance of <see cref="ValueTask"/> that represents the asynchronous execution.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="callback"/> is <see langword="null"/>.</exception>
+    public ValueTask<TResult> ExecuteAsync(
+        Func<CancellationToken, ValueTask<TResult>> callback,
+        CancellationToken cancellationToken = default)
+    {
+        Guard.NotNull(callback);
+
+        return Strategy.ExecuteAsync(callback, cancellationToken);
+    }
+
+    /// <summary>
+    /// Executes the specified outcome-based callback.
+    /// </summary>
+    /// <typeparam name="TState">The type of state associated with the callback.</typeparam>
+    /// <param name="callback">The user-provided callback.</param>
+    /// <param name="context">The context associated with the callback.</param>
+    /// <param name="state">The state associated with the callback.</param>
+    /// <returns>The instance of <see cref="ValueTask"/> that represents the asynchronous execution.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="callback"/> or <paramref name="context"/> is <see langword="null"/>.</exception>
+    /// <remarks>
+    /// This method is for advanced and high performance scenarios. The caller must make sure that the <paramref name="callback"/>
+    /// does not throw any exceptions. Instead, it converts them to <see cref="Outcome{TResult}"/>.
+    /// </remarks>
+    public ValueTask<Outcome<TResult>> ExecuteOutcomeAsync<TState>(
+        Func<ResilienceContext, TState, ValueTask<Outcome<TResult>>> callback,
+        ResilienceContext context,
+        TState state)
+    {
+        Guard.NotNull(callback);
+        Guard.NotNull(context);
+
+        return Strategy.ExecuteOutcomeAsync(callback, context, state);
+    }
+}
