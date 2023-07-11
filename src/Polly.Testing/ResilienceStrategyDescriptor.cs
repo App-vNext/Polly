adff8b699752a1f7bfbcf3727d38fbee3e056819
Martin Tomka <martintomka@microsoft.com>
2023-07-11 13:26:39 +0200

    Fix docs

--- src/Polly.Testing/ResilienceStrategyDescriptor.cs
+++ src/Polly.Testing/ResilienceStrategyDescriptor.cs
@@ -1,7 +1,7 @@
 ﻿namespace Polly.Testing;
 
 /// <summary>
-/// This class provides additional information about <see cref="ResilienceStrategy"/>.
+/// This class provides additional information about a <see cref="ResilienceStrategy"/>.
 /// </summary>
 /// <param name="Options">The options used by the resilience strategy, if any.</param>
 /// <param name="StrategyType">The type of the strategy.</param>
