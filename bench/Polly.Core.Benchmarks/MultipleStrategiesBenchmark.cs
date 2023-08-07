782fcac33c85fb3769ca1e2b8025d1f40e2aaf3c
Martin Tomka <martintomka@microsoft.com>
2023-08-07 09:41:46 +0200

    Improve `MultipleStrategiesBenchmark`

--- bench/Polly.Core.Benchmarks/MultipleStrategiesBenchmark.cs
+++ bench/Polly.Core.Benchmarks/MultipleStrategiesBenchmark.cs
@@ -9,6 +9,7 @@ public class MultipleStrategiesBenchmark
     private object? _strategyV8;
     private object? _strategyTelemetryV8;
     private ResilienceStrategy? _nonGeneric;
+    private ResilienceStrategy? _nonGenericTelemetry;
 
     [GlobalSetup]
     public void Setup()
@@ -17,20 +18,21 @@ public class MultipleStrategiesBenchmark
         _strategyV7 = Helper.CreateStrategyPipeline(PollyVersion.V7, false);
         _strategyV8 = Helper.CreateStrategyPipeline(PollyVersion.V8, false);
         _strategyTelemetryV8 = Helper.CreateStrategyPipeline(PollyVersion.V8, true);
-        _nonGeneric = Helper.CreateNonGenericStrategyPipeline();
+        _nonGeneric = Helper.CreateNonGenericStrategyPipeline(telemetry: false);
+        _nonGenericTelemetry = Helper.CreateNonGenericStrategyPipeline(telemetry: true);
     }
 
     [GlobalCleanup]
     public void Cleanup() => _meterListener?.Dispose();
 
     [Benchmark(Baseline = true)]
-    public ValueTask ExecuteStrategyPipeline_V7() => _strategyV7!.ExecuteAsync(PollyVersion.V7);
+    public ValueTask ExecuteStrategyPipeline_Generic_V7() => _strategyV7!.ExecuteAsync(PollyVersion.V7);
 
     [Benchmark]
-    public ValueTask ExecuteStrategyPipeline_V8() => _strategyV8!.ExecuteAsync(PollyVersion.V8);
+    public ValueTask ExecuteStrategyPipeline_Generic_V8() => _strategyV8!.ExecuteAsync(PollyVersion.V8);
 
     [Benchmark]
-    public ValueTask ExecuteStrategyPipeline_Telemetry_V8() => _strategyTelemetryV8!.ExecuteAsync(PollyVersion.V8);
+    public ValueTask ExecuteStrategyPipeline_GenericTelemetry_V8() => _strategyTelemetryV8!.ExecuteAsync(PollyVersion.V8);
 
     [Benchmark]
     public async ValueTask ExecuteStrategyPipeline_NonGeneric_V8()
@@ -38,6 +40,19 @@ public class MultipleStrategiesBenchmark
         var context = ResilienceContextPool.Shared.Get();
 
         await _nonGeneric!.ExecuteOutcomeAsync(
+            static (_, _) => new ValueTask<Outcome<string>>(Outcome.FromResult("dummy")),
+            context,
+            string.Empty).ConfigureAwait(false);
+
+        ResilienceContextPool.Shared.Return(context);
+    }
+
+    [Benchmark]
+    public async ValueTask ExecuteStrategyPipeline_NonGenericTelemetry_V8()
+    {
+        var context = ResilienceContextPool.Shared.Get();
+
+        await _nonGenericTelemetry!.ExecuteOutcomeAsync(
             static (_, _) => new ValueTask<Outcome<string>>(Outcome.FromResult("dummy")),
             context,
             string.Empty).ConfigureAwait(false);
