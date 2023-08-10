196da47cbb1946b70afcb62189e0875c7d0a2ccc
Martin Tomka <martintomka@microsoft.com>
2023-08-10 18:39:50 +0200

    cleanup

--- src/Polly.Core/Hedging/HedgingResilienceStrategy.cs
+++ src/Polly.Core/Hedging/HedgingResilienceStrategy.cs
@@ -97,7 +97,7 @@ internal sealed class HedgingResilienceStrategy<T> : ReactiveResilienceStrategy<
                 await HandleOnHedgingAsync(
                     context,
                     Outcome.FromResult<T>(default),
-                    new OnHedgingArguments(attempt, hasOutcome: false, executionTime: delay)).ConfigureAwait(context.ContinueOnCapturedContext);
+                    new OnHedgingArguments(attempt, hasOutcome: false, duration: delay)).ConfigureAwait(context.ContinueOnCapturedContext);
                 continue;
             }
 
