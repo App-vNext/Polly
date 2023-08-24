6ddc141d9f798d1533d4233625805397cee4e395
Martin Tomka <martintomka@microsoft.com>
2023-08-24 09:22:29 +0200

    fixes

--- test/Polly.Core.Tests/Utils/Pipeline/PipelineComponentFactoryTests.cs
+++ test/Polly.Core.Tests/Utils/Pipeline/PipelineComponentFactoryTests.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NSubstitute;
+﻿using NSubstitute;
 using Polly.Utils.Pipeline;
 
 namespace Polly.Core.Tests.Utils.Pipeline;
