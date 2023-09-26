01f567c648a1a4197fc4c1c9c20c9c4ec690e1f7
peter.csala <peter.csala@genesys.com>
2023-09-26 14:22:07 +0200

    Add antipattern to testing

--- docs/advanced/testing.md
+++ docs/advanced/testing.md
@@ -61,3 +61,76 @@ ResiliencePipelineDescriptor descriptor = pipeline.GetPipelineDescriptor();
 // ...
 ```
 <!-- endSnippet -->
+
+
+## Patterns and anti-patterns
+
+Over the years, many developers have used Polly in various ways. Some of these recurring patterns may not be ideal. This section highlights the recommended practices and those to avoid.
+
+### 1 - Recreating the policies inside unit tests
+
+Imagine that you have a class that receives a `ResiliencePipeline<T>` as a consturtor parameter for testability.
+
+❌ DON'T
+
+Recreate the strategey inside the unit test:
+
+<!-- snippet: testing-anti-pattern-1 -->
+```cs
+// Arrange
+var timeout = new ResiliencePipelineBuilder<HttpResponseMessage>()
+    .AddTimeout(TimeSpan.FromSeconds(1))
+    .Build();
+
+var mockDownstream = Substitute.For<IDownstream>();
+mockDownstream.CallDownstream(Arg.Any<CancellationToken>())
+.Returns((_) => { Thread.Sleep(5_000); return new HttpResponseMessage(); }, null);
+
+// Act
+var sut = new SUT(mockDownstream, timeout);
+await sut.RetrieveData();
+```
+<!-- endSnippet -->
+
+**Reasoning**:
+
+- Depending on the your strategy setup, the execution time might be greatly increased (from milliseconds to seconds).
+  - For example if you have a retry with 3 `MaxRetryAttempts` and 1 seconds `Delay`.
+- This violates one of the F.I.R.S.T testing principles, namely the **Fast** one.
+
+✅ DO
+
+Use `Empty` or mock `ExecuteAsync`:
+
+- If you want to test your to-be-decorated code like there is no strategy applied
+
+<!-- snippet: testing-pattern-1-1 -->
+```cs
+var timeoutMock = ResiliencePipeline<HttpResponseMessage>.Empty;
+```
+<!-- endSnippet -->
+
+- If you want to verify that your code handles success cases as expected
+
+<!-- snippet: testing-pattern-1-2 -->
+```cs
+var timeoutMock = Substitute.For<MockableResilienceStrategy>();
+timeoutMock.ExecuteAsync(Arg.Any<Func<CancellationToken, ValueTask<HttpResponseMessage>>>(), Arg.Any<CancellationToken>())
+        .Returns(new HttpResponseMessage(HttpStatusCode.Accepted));
+```
+<!-- endSnippet -->
+
+- If you want to verify that your code handles failure cases as expected
+
+<!-- snippet: testing-pattern-1-3 -->
+```cs
+var timeoutMock = Substitute.For<MockableResilienceStrategy>();
+timeoutMock.ExecuteAsync(Arg.Any<Func<CancellationToken, ValueTask<HttpResponseMessage>>>(), Arg.Any<CancellationToken>())
+        .ThrowsAsync(new TimeoutRejectedException());
+```
+<!-- endSnippet -->
+
+**Reasoning**:
+
+- The `MockableResilienceStrategy` is a derived class from `ResilienceStrategy` which exposes an `ExecuteAsync`
+  - BUT this can't be used as `ResiliencePipeline`
--- src/Snippets/Docs/Testing.cs
+++ src/Snippets/Docs/Testing.cs
@@ -1,4 +1,8 @@
-﻿using Polly.Retry;
+﻿using System.Net;
+using System.Net.Http;
+using NSubstitute;
+using NSubstitute.ExceptionExtensions;
+using Polly.Retry;
 using Polly.Testing;
 using Polly.Timeout;
 using Xunit;
@@ -57,5 +61,89 @@ internal static class Testing
         // ...
 
         #endregion
+    }
+
+    /// <summary>
+    /// A dummy interface to represent a downstream call.
+    /// </summary>
+    private interface IDownstream
+    {
+        /// <summary>
+        /// The method.
+        /// </summary>
+        /// <param name="token">The parameter.</param>
+        /// <returns>The result.</returns>
+        ValueTask<HttpResponseMessage> CallDownstream(CancellationToken token);
+    }
+
+    private class SUT
+    {
+        private readonly ResiliencePipeline<HttpResponseMessage> _pipeline;
+        public SUT(IDownstream downstream, ResiliencePipeline<HttpResponseMessage> pipeline)
+        {
+            _pipeline = pipeline;
+        }
+
+        public Task RetrieveData()
+        {
+            _ = _pipeline.ToString(); // Just to make it an instance method
+            return Task.CompletedTask;
+        }
+    }
+
+    public static async Task AntiPattern_1()
+    {
+        #region testing-anti-pattern-1
+        // Arrange
+        var timeout = new ResiliencePipelineBuilder<HttpResponseMessage>()
+            .AddTimeout(TimeSpan.FromSeconds(1))
+            .Build();
+
+        var mockDownstream = Substitute.For<IDownstream>();
+        mockDownstream.CallDownstream(Arg.Any<CancellationToken>())
+        .Returns((_) => { Thread.Sleep(5_000); return new HttpResponseMessage(); }, null);
+
+        // Act
+        var sut = new SUT(mockDownstream, timeout);
+        await sut.RetrieveData();
+        #endregion
+    }
+
+    public static void Pattern_11()
+    {
+        #region testing-pattern-1-1
+        var timeoutMock = ResiliencePipeline<HttpResponseMessage>.Empty;
+        #endregion
+    }
+
+    public static void Pattern_12()
+    {
+        #region testing-pattern-1-2
+        var timeoutMock = Substitute.For<MockableResilienceStrategy>();
+        timeoutMock.ExecuteAsync(Arg.Any<Func<CancellationToken, ValueTask<HttpResponseMessage>>>(), Arg.Any<CancellationToken>())
+                .Returns(new HttpResponseMessage(HttpStatusCode.Accepted));
+        #endregion
+    }
+
+    public static void Pattern_13()
+    {
+        #region testing-pattern-1-3
+        var timeoutMock = Substitute.For<MockableResilienceStrategy>();
+        timeoutMock.ExecuteAsync(Arg.Any<Func<CancellationToken, ValueTask<HttpResponseMessage>>>(), Arg.Any<CancellationToken>())
+                .ThrowsAsync(new TimeoutRejectedException());
+        #endregion
+    }
+
+    public abstract class MockableResilienceStrategy : ResilienceStrategy
+    {
+        public virtual async ValueTask<TResult?> ExecuteAsync<TResult>(Func<CancellationToken, ValueTask<TResult>> callback, CancellationToken token)
+        {
+            return default;
+        }
+
+        protected override ValueTask<Outcome<TResult>> ExecuteCore<TResult, TState>(Func<ResilienceContext, TState, ValueTask<Outcome<TResult>>> callback, ResilienceContext context, TState state)
+        {
+            return default;
+        }
     }
 }
