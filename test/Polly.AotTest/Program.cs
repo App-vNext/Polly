caaeba94e7715b6f11f13251901cafeb7c760eb8
martincostello <martin@martincostello.com>
2023-10-30 10:37:27 +0000

    Simplify AoT repro
    Turns out that the issue is detected without the additional hint of explicitly exercising the problematic code.

--- test/Polly.AotTest/Program.cs
+++ test/Polly.AotTest/Program.cs
@@ -1,8 +1,1 @@
-using Polly.Utils.Pipeline;
-
-// See https://github.com/App-vNext/Polly/issues/1732#issuecomment-1782466692.
-// This code is needed as a workaround until https://github.com/dotnet/runtime/issues/94131 is resolved.
-var pipeline = CompositeComponent.Create(new[] { PipelineComponent.Empty, PipelineComponent.Empty }, null!, null!);
-await pipeline.ExecuteCore<int, int>((state, context) => default, default!, default);
-
 Console.WriteLine("Hello Polly!");
