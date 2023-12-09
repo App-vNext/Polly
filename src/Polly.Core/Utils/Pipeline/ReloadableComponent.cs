91c7e75d8e0599b37910fde3923c74f3315ac87c
martincostello <martin@martincostello.com>
2023-12-09 13:53:13 +0000

    Fix code coverage
    Removing the "useless primary constructor" breaks code coverage as the empty records appears to be unused (when it is), so add it back and suppress the warning instead.

--- src/Polly.Core/Utils/Pipeline/ReloadableComponent.cs
+++ src/Polly.Core/Utils/Pipeline/ReloadableComponent.cs
@@ -106,7 +106,9 @@ internal sealed class ReloadableComponent : PipelineComponent
     internal record DisposedFailedArguments(Exception Exception);
 
 #pragma warning disable S2094 // Classes should not be empty
-    internal record OnReloadArguments;
+#pragma warning disable S3253 // Constructor and destructor declarations should not be redundant
+    internal record OnReloadArguments();
+#pragma warning restore S3253 // Constructor and destructor declarations should not be redundant
 #pragma warning restore S2094 // Classes should not be empty
 
     internal record Entry(PipelineComponent Component, List<CancellationToken> ReloadTokens, ResilienceStrategyTelemetry Telemetry);
