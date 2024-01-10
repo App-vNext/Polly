48f64d340c348e0e07412dfe371539781e3ae53b
martintmk <103487740+martintmk@users.noreply.github.com>
2024-01-10 20:30:40 +0100

    Decrease the minimum allowed timeout (#1888)

--- test/Polly.Core.Tests/Timeout/TimeoutTestUtils.cs
+++ test/Polly.Core.Tests/Timeout/TimeoutTestUtils.cs
@@ -15,12 +15,15 @@ public static class TimeoutTestUtils
         TimeSpan.Zero,
         TimeSpan.FromSeconds(-1),
         TimeSpan.FromHours(25),
+        TimeSpan.FromMilliseconds(9),
     };
 
     public static readonly TheoryData<TimeSpan> ValidTimeouts = new()
     {
         TimeSpan.FromSeconds(1),
         TimeSpan.FromHours(1),
+        TimeSpan.FromMilliseconds(10),
+
     };
 #pragma warning restore IDE0028
 }
