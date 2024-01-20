6ed31e64a857d05fac440bf5ee0210626e28349b
Martin Tomka <martintomka@microsoft.com>
2024-01-20 20:40:04 +0100

    Introduce `FaultGenerator` and `OutcomeGenerator<T>`

--- src/Polly.Core/Simmy/Outcomes/OutcomeStrategyOptions.cs
+++ src/Polly.Core/Simmy/Outcomes/OutcomeStrategyOptions.cs
@@ -1,6 +1,24 @@
-﻿namespace Polly.Simmy.Outcomes;
+﻿using System.ComponentModel.DataAnnotations;
 
-/// <inheritdoc/>
-public class OutcomeStrategyOptions : OutcomeStrategyOptions<object>
+namespace Polly.Simmy.Outcomes;
+
+/// <summary>
+/// Represents the options for the Outcome chaos strategy.
+/// </summary>
+/// <typeparam name="TResult">The type of the outcome that was injected.</typeparam>
+public class OutcomeStrategyOptions<TResult> : MonkeyStrategyOptions
 {
+    /// <summary>
+    /// Gets or sets the delegate that's raised when the outcome is injected.
+    /// </summary>
+    /// <remarks>
+    /// Defaults to <see langword="null"/>.
+    /// </remarks>
+    public Func<OnOutcomeInjectedArguments<TResult>, ValueTask>? OnOutcomeInjected { get; set; }
+
+    /// <summary>
+    /// Gets or sets the outcome generator to be injected for a given execution.
+    /// </summary>
+    [Required]
+    public Func<OutcomeGeneratorArguments, ValueTask<Outcome<TResult>?>> OutcomeGenerator { get; set; } = default!;
 }
---
+++ test/Polly.Core.Tests/Simmy/Outcomes/OutcomeGeneratorTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Polly.Simmy.Outcomes;
+
+namespace Polly.Core.Tests.Simmy.Outcomes;
+
+public class OutcomeGeneratorTests
+{
+    [Fact]
+    public void AddException_Generic_Ok()
+    {
+        var generator = new OutcomeGenerator<string>();
+
+        generator.AddException<InvalidOperationException>();
+
+        Generate(generator)!.Value.Exception.Should().BeOfType<InvalidOperationException>();
+    }
+
+    [Fact]
+    public void AddException_Factory_Ok()
+    {
+        var generator = new OutcomeGenerator<string>();
+
+        generator.AddException(() => new InvalidOperationException());
+
+        Generate(generator)!.Value.Exception.Should().BeOfType<InvalidOperationException>();
+    }
+
+    [Fact]
+    public void AddException_FactoryWithResilienceContext_Ok()
+    {
+        var generator = new OutcomeGenerator<string>();
+
+        generator.AddException(context =>
+        {
+            context.Should().NotBeNull();
+
+            return new InvalidOperationException();
+        });
+
+        Generate(generator)!.Value.Exception.Should().BeOfType<InvalidOperationException>();
+    }
+
+    [Fact]
+    public void AddResult_Factory_Ok()
+    {
+        var generator = new OutcomeGenerator<string>();
+
+        generator.AddResult(() =>
+        {
+            return "dummy";
+        });
+
+        Generate(generator)!.Value.Result.Should().Be("dummy");
+    }
+
+    [Fact]
+    public void AddResult_FactoryWithResilienceContext_Ok()
+    {
+        var generator = new OutcomeGenerator<string>();
+
+        generator.AddResult(context =>
+        {
+            context.Should().NotBeNull();
+
+            return "dummy";
+        });
+
+        Generate(generator)!.Value.Result.Should().Be("dummy");
+    }
+
+    private static Outcome<string>? Generate(OutcomeGenerator<string> generator)
+    {
+        Func<OutcomeGeneratorArguments, ValueTask<Outcome<string>?>> func = generator;
+
+        return func(new OutcomeGeneratorArguments(ResilienceContextPool.Shared.Get())).AsTask().Result;
+    }
+}
