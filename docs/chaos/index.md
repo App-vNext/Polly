4575337f7a18a65420facf8408e423b74f7b9256
Martin Tomka <martintomka@microsoft.com>
2024-01-22 14:46:57 +0100

    [Docs] Add general usage section to chaos strategies

--- docs/chaos/index.md
+++ docs/chaos/index.md
@@ -7,6 +7,37 @@
 
 ![Simmy](../media/simmy-logo.png)
 
+## Usage
+
+<!-- snippet: chaos-usage -->
+```cs
+var builder = new ResiliencePipelineBuilder<HttpResponseMessage>();
+
+// First, configure regular resilience strategies
+builder
+    .AddConcurrencyLimiter(10, 100)
+    .AddRetry(new())
+    .AddCircuitBreaker(new())
+    .AddTimeout(TimeSpan.FromSeconds(5));
+
+// Finally, configure chaos strategies if you want to inject chaos.
+// These should come after the regular resilience strategies.
+
+// 2% of invocations will be injected with chaos
+const double InjectionRate = 0.02;
+
+builder
+    // Inject a chaos latency to executions
+    .AddChaosLatency(InjectionRate, TimeSpan.FromMinutes(1))
+    // Inject a chaos fault to executions
+    .AddChaosFault(InjectionRate, () => new InvalidOperationException("Injected by chaos strategy!"))
+    // Inject a chaos outcome to executions
+    .AddChaosResult(InjectionRate, () => new HttpResponseMessage(System.Net.HttpStatusCode.InternalServerError))
+    // Inject a chaos behavior to executions
+    .AddChaosBehavior(0.001, () => RestartRedisAsync());
+```
+<!-- endSnippet -->
+
 ## Motivation
 
 There are a lot of questions when it comes to chaos engineering and making sure that a system is actually ready to face the worst possible scenarios:
---
+++ src/Snippets/Docs/Chaos.Index.cs
@@ -0,0 +1,41 @@
+using System.Net.Http;
+using Polly.Simmy;
+
+namespace Snippets.Docs;
+
+internal static partial class Chaos
+{
+    public static void Usage()
+    {
+        #region chaos-usage
+
+        var builder = new ResiliencePipelineBuilder<HttpResponseMessage>();
+
+        // First, configure regular resilience strategies
+        builder
+            .AddConcurrencyLimiter(10, 100)
+            .AddRetry(new())
+            .AddCircuitBreaker(new())
+            .AddTimeout(TimeSpan.FromSeconds(5));
+
+        // Finally, configure chaos strategies if you want to inject chaos.
+        // These should come after the regular resilience strategies.
+
+        // 2% of invocations will be injected with chaos
+        const double InjectionRate = 0.02;
+
+        builder
+            // Inject a chaos latency to executions
+            .AddChaosLatency(InjectionRate, TimeSpan.FromMinutes(1))
+            // Inject a chaos fault to executions
+            .AddChaosFault(InjectionRate, () => new InvalidOperationException("Injected by chaos strategy!"))
+            // Inject a chaos outcome to executions
+            .AddChaosResult(InjectionRate, () => new HttpResponseMessage(System.Net.HttpStatusCode.InternalServerError))
+            // Inject a chaos behavior to executions
+            .AddChaosBehavior(0.001, () => RestartRedisAsync());
+
+        #endregion
+    }
+
+    private static ValueTask RestartRedisAsync() => ValueTask.CompletedTask;
+}
