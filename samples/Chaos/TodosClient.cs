29f1157b03de727f4e6286d1ae52438c6f3c8c9b
Martin Tomka <martintomka@microsoft.com>
2024-02-05 22:26:46 +0100

    Simplify the code

--- samples/Chaos/TodosClient.cs
+++ samples/Chaos/TodosClient.cs
@@ -2,11 +2,6 @@
 
 public class TodosClient(HttpClient client)
 {
-    public async Task<IEnumerable<TodoModel>?> GetTodosAsync(CancellationToken cancellationToken)
-    {
-        using var request = new HttpRequestMessage(HttpMethod.Get, "/todos");
-        using var response = await client.SendAsync(request, cancellationToken);
-        response.EnsureSuccessStatusCode();
-        return (await response.Content.ReadFromJsonAsync<List<TodoModel>>(cancellationToken))!.Take(10);
-    }
+    public async Task<IEnumerable<TodoModel>> GetTodosAsync(CancellationToken cancellationToken)
+        => await client.GetFromJsonAsync<IEnumerable<TodoModel>>("/todos", cancellationToken) ?? [];
 }
