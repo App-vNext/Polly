69c8d517790e4d4512fea46768766d7a82b08c6d
martincostello <martin@martincostello.com>
2024-04-12 09:50:01 +0100

    Fix IDE0057 warnings
    Fix IDE0057 warnings identified in #2003.

--- src/Polly/Utilities/KeyHelper.cs
+++ src/Polly/Utilities/KeyHelper.cs
@@ -6,5 +6,9 @@ internal static class KeyHelper
     private const int GuidPartLength = 8;
 
     public static string GuidPart() =>
+#if NET6_0_OR_GREATER
+        Guid.NewGuid().ToString()[..GuidPartLength];
+#else
         Guid.NewGuid().ToString().Substring(0, GuidPartLength);
+#endif
 }
