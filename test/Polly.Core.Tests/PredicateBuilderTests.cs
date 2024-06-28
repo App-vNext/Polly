2b44e7a655e4cf24f5135efcce50ac713c13abea
Martin Costello <martin@martincostello.com>
2024-06-28 10:20:42 +0100

    Handle nested inner exceptions (#2166)
    - Replicate behaviour of v7 policies and recursively check all inner exceptions and `AggregateException` to handle inner exceptions.
    - Refactor predicate builder test cases to make a bit more readable.
    - Use collection expression.

--- test/Polly.Core.Tests/PredicateBuilderTests.cs
+++ test/Polly.Core.Tests/PredicateBuilderTests.cs
@@ -9,24 +9,47 @@ public class PredicateBuilderTests
 {
     public static TheoryData<Action<PredicateBuilder<string>>, Outcome<string>, bool> HandleResultData = new()
     {
-        { builder => builder.HandleResult("val"), Outcome.FromResult("val"), true },
-        { builder => builder.HandleResult("val"), Outcome.FromResult("val2"), false },
-        { builder => builder.HandleResult("val"), Outcome.FromException<string>(new InvalidOperationException()), false },
-        { builder => builder.HandleResult("val", StringComparer.OrdinalIgnoreCase) ,Outcome.FromResult("VAL"), true },
-        { builder => builder.HandleResult(r => r == "val"), Outcome.FromResult("val"), true },
-        { builder => builder.HandleResult(r => r == "val2"), Outcome.FromResult("val"), false },
-        { builder => builder.Handle<InvalidOperationException>(), Outcome.FromException<string>(new InvalidOperationException()), true },
-        { builder => builder.Handle<InvalidOperationException>(), Outcome.FromException<string>(new FormatException()), false },
-        { builder => builder.Handle<InvalidOperationException>(e => false), Outcome.FromException<string>(new InvalidOperationException()), false },
-        { builder => builder.HandleInner<InvalidOperationException>(e => false), Outcome.FromException<string>(new InvalidOperationException()), false },
-        { builder => builder.HandleInner<InvalidOperationException>(), Outcome.FromResult("value"), false },
-        { builder => builder.Handle<InvalidOperationException>(), Outcome.FromResult("value"), false },
-        { builder => builder.Handle<InvalidOperationException>().HandleResult("value"), Outcome.FromResult("value"), true },
-        { builder => builder.Handle<InvalidOperationException>().HandleResult("value"), Outcome.FromResult("value2"), false },
-        { builder => builder.HandleInner<FormatException>(), Outcome.FromException<string>(new InvalidOperationException("dummy", new FormatException() )), true },
-        { builder => builder.HandleInner<ArgumentNullException>(e => false), Outcome.FromException<string>(new InvalidOperationException("dummy", new FormatException() )), false },
-        { builder => builder.HandleInner<FormatException>(e => e.Message == "m"), Outcome.FromException<string>(new InvalidOperationException("dummy", new FormatException("m") )), true },
-        { builder => builder.HandleInner<FormatException>(e => e.Message == "x"), Outcome.FromException<string>(new InvalidOperationException("dummy", new FormatException("m") )), false },
+        { builder => builder.HandleResult("val"), CreateOutcome("val"), true },
+        { builder => builder.HandleResult("val"), CreateOutcome("val2"), false },
+        { builder => builder.HandleResult("val"), CreateOutcome(new InvalidOperationException()), false },
+        { builder => builder.HandleResult("val", StringComparer.OrdinalIgnoreCase), CreateOutcome("VAL"), true },
+        { builder => builder.HandleResult(r => r == "val"), CreateOutcome("val"), true },
+        { builder => builder.HandleResult(r => r == "val2"), CreateOutcome("val"), false },
+        { builder => builder.Handle<InvalidOperationException>(), CreateOutcome(new InvalidOperationException()), true },
+        { builder => builder.Handle<InvalidOperationException>(), CreateOutcome(new FormatException()), false },
+        { builder => builder.Handle<InvalidOperationException>(e => false), CreateOutcome(new InvalidOperationException()), false },
+        { builder => builder.HandleInner<InvalidOperationException>(e => false), CreateOutcome(new InvalidOperationException()), false },
+        { builder => builder.HandleInner<InvalidOperationException>(), CreateOutcome("value"), false },
+        { builder => builder.Handle<InvalidOperationException>(), CreateOutcome("value"), false },
+        { builder => builder.Handle<InvalidOperationException>().HandleResult("value"), CreateOutcome("value"), true },
+        { builder => builder.Handle<InvalidOperationException>().HandleResult("value"), CreateOutcome("value2"), false },
+        { builder => builder.HandleInner<FormatException>(), CreateOutcome(new InvalidOperationException("dummy", new FormatException() )), true },
+        { builder => builder.HandleInner<ArgumentNullException>(e => false), CreateOutcome(new InvalidOperationException("dummy", new FormatException() )), false },
+        { builder => builder.HandleInner<FormatException>(e => e.Message == "m"), CreateOutcome(new InvalidOperationException("dummy", new FormatException("m") )), true },
+        { builder => builder.HandleInner<FormatException>(e => e.Message == "x"), CreateOutcome(new InvalidOperationException("dummy", new FormatException("m") )), false },
+#pragma warning disable CA2201
+        //// See https://github.com/App-vNext/Polly/issues/2161
+        { builder => builder.HandleInner<InvalidOperationException>(), CreateOutcome(new InvalidOperationException("1")), true },
+        { builder => builder.HandleInner<InvalidOperationException>(), CreateOutcome(new Exception("1", new InvalidOperationException("2"))), true },
+        { builder => builder.HandleInner<InvalidOperationException>(), CreateOutcome(new FormatException("1", new InvalidOperationException("2"))), true },
+        { builder => builder.HandleInner<InvalidOperationException>(), CreateOutcome(new Exception("1", new Exception("2", new InvalidOperationException("3")))), true },
+        { builder => builder.HandleInner<InvalidOperationException>(), CreateOutcome(new AggregateException("1", new Exception("2a"), new InvalidOperationException("2b"))), true },
+        { builder => builder.HandleInner<InvalidOperationException>(), CreateOutcome(new AggregateException("1", new Exception("2", new InvalidOperationException("3")))), true },
+        { builder => builder.HandleInner<InvalidOperationException>(), CreateOutcome(new AggregateException("1", new FormatException("2", new NotSupportedException("3")))), false },
+        { builder => builder.HandleInner<InvalidOperationException>(), CreateOutcome(new AggregateException("1")), false },
+        { builder => builder.HandleInner<InvalidOperationException>(ex => ex.Message is "3"), CreateOutcome(new AggregateException("1", new FormatException("2", new NotSupportedException("3")))), false },
+        { builder => builder.HandleInner<InvalidOperationException>(ex => ex.Message is "unreachable"), CreateOutcome(new AggregateException("1", new FormatException("2", new NotSupportedException("3")))), false },
+        { builder => builder.HandleInner<InvalidOperationException>(ex => ex.Message is "1"), CreateOutcome(new InvalidOperationException("1")), true },
+        { builder => builder.HandleInner<InvalidOperationException>(ex => ex.Message is "2"), CreateOutcome(new Exception("1", new InvalidOperationException("2"))), true },
+        { builder => builder.HandleInner<InvalidOperationException>(ex => ex.Message is "3"), CreateOutcome(new Exception("1", new Exception("2", new InvalidOperationException("3")))), true },
+        { builder => builder.HandleInner<InvalidOperationException>(ex => ex.Message is "2b"), CreateOutcome(new AggregateException("1", new Exception("2a"), new InvalidOperationException("2b"))), true },
+        { builder => builder.HandleInner<InvalidOperationException>(ex => ex.Message is "3"), CreateOutcome(new AggregateException("1", new Exception("2", new InvalidOperationException("3")))), true },
+        { builder => builder.HandleInner<InvalidOperationException>(ex => ex.Message is "unreachable"), CreateOutcome(new InvalidOperationException("1")), false },
+        { builder => builder.HandleInner<InvalidOperationException>(ex => ex.Message is "unreachable"), CreateOutcome(new Exception("1", new InvalidOperationException("2"))), false },
+        { builder => builder.HandleInner<InvalidOperationException>(ex => ex.Message is "unreachable"), CreateOutcome(new Exception("1", new Exception("2", new InvalidOperationException("3")))), false },
+        { builder => builder.HandleInner<InvalidOperationException>(ex => ex.Message is "unreachable"), CreateOutcome(new AggregateException("1", new Exception("2a"), new InvalidOperationException("2b"))), false },
+        { builder => builder.HandleInner<InvalidOperationException>(ex => ex.Message is "unreachable"), CreateOutcome(new AggregateException("1", new Exception("2", new InvalidOperationException("3")))), false },
+#pragma warning restore CA2201
     };
 
     [Fact]
@@ -66,7 +89,7 @@ public class PredicateBuilderTests
             ShouldHandle = new PredicateBuilder<string>().HandleResult("error")
         };
 
-        var handled = await options.ShouldHandle(new RetryPredicateArguments<string>(ResilienceContextPool.Shared.Get(), Outcome.FromResult("error"), 0));
+        var handled = await options.ShouldHandle(new RetryPredicateArguments<string>(ResilienceContextPool.Shared.Get(), CreateOutcome("error"), 0));
 
         handled.Should().BeTrue();
     }
@@ -79,7 +102,7 @@ public class PredicateBuilderTests
             ShouldHandle = new PredicateBuilder<string>().HandleResult("error")
         };
 
-        var handled = await options.ShouldHandle(new(ResilienceContextPool.Shared.Get(), Outcome.FromResult("error")));
+        var handled = await options.ShouldHandle(new(ResilienceContextPool.Shared.Get(), CreateOutcome("error")));
 
         handled.Should().BeTrue();
     }
@@ -92,7 +115,7 @@ public class PredicateBuilderTests
             ShouldHandle = new PredicateBuilder<string>().HandleResult("error")
         };
 
-        var handled = await options.ShouldHandle(new(ResilienceContextPool.Shared.Get(), Outcome.FromResult("error")));
+        var handled = await options.ShouldHandle(new(ResilienceContextPool.Shared.Get(), CreateOutcome("error")));
 
         handled.Should().BeTrue();
     }
@@ -105,8 +128,14 @@ public class PredicateBuilderTests
             ShouldHandle = new PredicateBuilder<string>().HandleResult("error")
         };
 
-        var handled = await options.ShouldHandle(new(ResilienceContextPool.Shared.Get(), Outcome.FromResult("error")));
+        var handled = await options.ShouldHandle(new(ResilienceContextPool.Shared.Get(), CreateOutcome("error")));
 
         handled.Should().BeTrue();
     }
+
+    private static Outcome<string> CreateOutcome(Exception exception)
+        => Outcome.FromException<string>(exception);
+
+    private static Outcome<string> CreateOutcome(string result)
+        => Outcome.FromResult(result);
 }
