87cfed7ea00fa1b481e036bc7e477024544800cf
ZLoo <kozlov.s.v.1992@gmail.com>
2024-07-21 21:22:53 +0300

    Fix CA1062 warnings (#2219)
    Fix CA1062 warnings for `NoOpPolicy`.

--- test/Polly.Specs/NoOp/NoOpSpecs.cs
+++ test/Polly.Specs/NoOp/NoOpSpecs.cs
@@ -3,6 +3,25 @@
 public class NoOpSpecs
 {
     [Fact]
+    public void Should_throw_when_action_is_null()
+    {
+        var flags = BindingFlags.NonPublic | BindingFlags.Instance;
+        Func<Context, CancellationToken, EmptyStruct> action = null!;
+
+        var instance = Activator.CreateInstance(typeof(NoOpPolicy), true)!;
+        var instanceType = instance.GetType();
+        var methods = instanceType.GetMethods(flags);
+
+        var methodInfo = methods.First(method => method is { Name: "Implementation", ReturnType.Name: "TResult" });
+        var generic = methodInfo.MakeGenericMethod(typeof(EmptyStruct));
+        var func = () => generic.Invoke(instance, [action, new Context(), CancellationToken.None]);
+
+        var exceptionAssertions = func.Should().Throw<TargetInvocationException>();
+        exceptionAssertions.And.Message.Should().Be("Exception has been thrown by the target of an invocation.");
+        exceptionAssertions.WithInnerException<ArgumentNullException>("action");
+    }
+
+    [Fact]
     public void Should_execute_user_delegate()
     {
         NoOpPolicy policy = Policy.NoOp();
