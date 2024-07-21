69e0e807a0e1706958d2bc5fd3fb0c116eb65c0d
ZLoo <kozlov.s.v.1992@gmail.com>
2024-07-21 21:52:26 +0300

    Fix CA1062 warnings (#2220)
    Fix warnings for `AsyncNoOpPolicy`.

--- test/Polly.Specs/NoOp/NoOpTResultAsyncSpecs.cs
+++ test/Polly.Specs/NoOp/NoOpTResultAsyncSpecs.cs
@@ -3,6 +3,24 @@
 public class NoOpTResultAsyncSpecs
 {
     [Fact]
+    public void Should_throw_when_action_is_null()
+    {
+        var flags = BindingFlags.NonPublic | BindingFlags.Instance;
+        Func<Context, CancellationToken, Task<EmptyStruct>> action = null!;
+
+        var instance = Activator.CreateInstance(typeof(AsyncNoOpPolicy<EmptyStruct>), true)!;
+        var instanceType = instance.GetType();
+        var methods = instanceType.GetMethods(flags);
+        var methodInfo = methods.First(method => method is { Name: "ImplementationAsync", ReturnType.Name: "Task`1" });
+
+        var func = () => methodInfo.Invoke(instance, [action, new Context(), CancellationToken.None, false]);
+
+        var exceptionAssertions = func.Should().Throw<TargetInvocationException>();
+        exceptionAssertions.And.Message.Should().Be("Exception has been thrown by the target of an invocation.");
+        exceptionAssertions.WithInnerException<ArgumentNullException>("action");
+    }
+
+    [Fact]
     public async Task Should_execute_user_delegate()
     {
         var policy = Policy.NoOpAsync<int?>();
