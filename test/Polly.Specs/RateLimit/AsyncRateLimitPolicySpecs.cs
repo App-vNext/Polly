ed1c79be1c0ce774072c123b52ac51e21be798fb
ZLoo <kozlov.s.v.1992@gmail.com>
2024-07-22 11:14:17 +0300

    Fix CA1062 warnings (#2223)
    Fix warnings for `AsyncRateLimitPolicy`.

--- test/Polly.Specs/RateLimit/AsyncRateLimitPolicySpecs.cs
+++ test/Polly.Specs/RateLimit/AsyncRateLimitPolicySpecs.cs
@@ -31,4 +31,30 @@ public class AsyncRateLimitPolicySpecs : RateLimitPolicySpecsBase, IDisposable
             throw new InvalidOperationException("Unexpected policy type in test construction.");
         }
     }
+
+    [Fact]
+    public void Should_throw_when_action_is_null()
+    {
+        var flags = BindingFlags.NonPublic | BindingFlags.Instance;
+        Func<Context, CancellationToken, Task<EmptyStruct>> action = null!;
+        IRateLimiter rateLimiter = RateLimiterFactory.Create(TimeSpan.FromSeconds(1), 1);
+
+        var instance = Activator.CreateInstance(
+            typeof(AsyncRateLimitPolicy),
+            flags,
+            null,
+            [rateLimiter],
+            null)!;
+        var instanceType = instance.GetType();
+        var methods = instanceType.GetMethods(flags);
+        var methodInfo = methods.First(method => method is { Name: "ImplementationAsync", ReturnType.Name: "Task`1" });
+        var generic = methodInfo.MakeGenericMethod(typeof(EmptyStruct));
+
+        var func = () => generic.Invoke(instance, [action, new Context(), CancellationToken.None, false]);
+
+        var exceptionAssertions = func.Should().Throw<TargetInvocationException>();
+        exceptionAssertions.And.Message.Should().Be("Exception has been thrown by the target of an invocation.");
+        exceptionAssertions.And.InnerException.Should().BeOfType<ArgumentNullException>()
+            .Which.ParamName.Should().Be("action");
+    }
 }
