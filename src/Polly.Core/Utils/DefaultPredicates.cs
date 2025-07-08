13d124a9cd939f809152702248f957d3c8b90825
martincostello <martin@martincostello.com>
2025-07-08 13:57:05 +0100

    Performance tweaks
    - Avoid LINQ to create components.
    - Do not recreate `Outcome<T>` for chaos.
    - Avoid predicate allocation.
    - Use `ArgumentNullException.ThrowIfNull()` where possible.
    - Add throw helper for `ObjectDisposedException`.
    - Remove unused `Stopwatch`.
    - Use `Volatile.Read` instead of `Interlocked.CompareExchange()`.
    
    Cherry-picked from #2664.
    
    Co-Authored-By: Pent Ploompuu <kaalikas@gmail.com>

--- src/Polly.Core/Utils/DefaultPredicates.cs
+++ src/Polly.Core/Utils/DefaultPredicates.cs
@@ -3,10 +3,5 @@
 internal static class DefaultPredicates<TArgs, TResult>
     where TArgs : IOutcomeArguments<TResult>
 {
-    public static readonly Func<TArgs, ValueTask<bool>> HandleOutcome = args => args.Outcome.Exception switch
-    {
-        OperationCanceledException => PredicateResult.False(),
-        Exception => PredicateResult.True(),
-        _ => PredicateResult.False(),
-    };
+    public static readonly Func<TArgs, ValueTask<bool>> HandleOutcome = args => new(args.Outcome.Exception is { } and not OperationCanceledException);
 }
