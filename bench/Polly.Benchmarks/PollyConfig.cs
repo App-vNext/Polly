1a0e5fd2954feec353ca7eb441479956cb45edd5
martincostello <martin@martincostello.com>
2025-10-31 11:30:44 +0000

    Remove obsolete option
    Remove usage of `WithNuGet()`.

--- bench/Polly.Benchmarks/PollyConfig.cs
+++ bench/Polly.Benchmarks/PollyConfig.cs
@@ -4,30 +4,7 @@ internal class PollyConfig : ManualConfig
 {
     public PollyConfig()
     {
-        var job = Job.Default;
-
         AddDiagnoser(BenchmarkDotNet.Diagnosers.MemoryDiagnoser.Default);
-
-        AddJob(PollyJob(job, useNuGet: true).AsBaseline());
-        AddJob(PollyJob(job, useNuGet: false));
-    }
-
-    private static Job PollyJob(Job job, bool useNuGet)
-    {
-        var result = job
-            .WithId("Polly" + (useNuGet ? string.Empty : "-dev"))
-            .WithArguments(
-            new[]
-            {
-                new MsBuildArgument("/p:BenchmarkFromNuGet=" + useNuGet),
-                new MsBuildArgument("/p:SignAssembly=false"),
-            });
-
-        if (useNuGet)
-        {
-            result = result.WithNuGet("Polly", "7.2.4");
-        }
-
-        return result;
+        AddJob(Job.Default);
     }
 }
