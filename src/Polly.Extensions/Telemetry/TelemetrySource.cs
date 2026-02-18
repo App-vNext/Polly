738e858c93f0ffd1b1a44c258fe67c4a01fe5f1a
martincostello <martin@martincostello.com>
2026-02-18 15:26:28 +0000

    Move Meter and add ActivitySource
    - Move the `Meter` to its own container class.
    - Use assembly version for the `Meter` instead of `1.0`.
    - Add `ActivitySource` for future use.

--- src/Polly.Extensions/Telemetry/TelemetrySource.cs
+++ src/Polly.Extensions/Telemetry/TelemetrySource.cs
@@ -11,8 +11,14 @@ internal sealed class TelemetrySource
     private TelemetrySource()
     {
         var version = GetVersion();
+
+        // In .NET 10+ we could use ActivitySourceOptions and
+        // MeterOptions to create these and provide a schema URL.
+        ActivitySource = new(Name, version);
         Meter = new(Name, version);
     }
+
+    public ActivitySource ActivitySource { get; }
 
     public Meter Meter { get; }
 
--- test/Polly.Extensions.Tests/Telemetry/TelemetrySourceTests.cs
+++ test/Polly.Extensions.Tests/Telemetry/TelemetrySourceTests.cs
@@ -5,7 +5,7 @@ namespace Polly.Extensions.Tests.Telemetry;
 public static class TelemetrySourceTests
 {
     [Fact]
-    public static void TelemetrySource_CreatesMeter()
+    public static void TelemetrySource_CreatesActivitySourceAndMeter()
     {
         var source = TelemetrySource.Instance;
 
@@ -17,6 +17,14 @@ public static class TelemetrySourceTests
         source.Meter.Version.ShouldNotContain('-');
         source.Meter.Version.ShouldNotContain('+');
         Version.TryParse(source.Meter.Version, out var version).ShouldBeTrue();
+        version.ShouldBeGreaterThan(new(0, 0, 0));
+
+        source.ActivitySource.ShouldNotBeNull();
+        source.ActivitySource.Name.ShouldBe("Polly");
+        source.ActivitySource.Version.ShouldNotBeNullOrEmpty();
+        source.ActivitySource.Version.ShouldNotContain('-');
+        source.ActivitySource.Version.ShouldNotContain('+');
+        Version.TryParse(source.ActivitySource.Version, out version).ShouldBeTrue();
         version.ShouldBeGreaterThan(new(0, 0, 0));
     }
 }
