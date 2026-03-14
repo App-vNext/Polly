0dc9faaf166882526e04dadcb12d62f6f4254a25
martincostello <martin@martincostello.com>
2026-03-14 15:52:41 +0000

    Telemetry refactoring
    Cherry-pick changes from #2936:
    
    - Move `Meter` to `TelemetrySource` class.
    - Use Polly's actual version for the meter's version.
    - Extend some test coverage.
    - Apply some minor code style refactoring.

---
+++ test/Polly.Extensions.Tests/Telemetry/TelemetrySourceTests.cs
@@ -0,0 +1,19 @@
+﻿using Polly.Telemetry;
+
+namespace Polly.Extensions.Tests.Telemetry;
+
+public static class TelemetrySourceTests
+{
+    [Fact]
+    public static void TelemetrySource_CreatesActivitySourceAndMeter()
+    {
+        var source = TelemetrySource.Instance;
+
+        source.ShouldNotBeNull();
+
+        source.Meter.ShouldNotBeNull();
+        source.Meter.Name.ShouldBe("Polly");
+        source.Meter.Version.ShouldNotBeNullOrEmpty();
+        Version.TryParse(source.Meter.Version, out _).ShouldBeTrue();
+    }
+}
